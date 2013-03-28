8976732209e762dde22381008fc1b79474471bb8
ggopu <devnull@localhost>
2013-03-28 04:03:14 -0700

    Created the Test bench For Vector Premitive Functions

---
+++ source/test/UnitTest.cpp
@@ -0,0 +1,230 @@
+#include "UnitTest.h"
+
+//Compare the Two given Buffers with Binary Mode
+int UnitTest::CompareBuffer (unsigned char * Buff_one, unsigned char * Buff_two)
+{
+	int Buff_one_length;
+	int Buff_two_length;
+
+	Buff_one_length = strlen((char *)Buff_one);
+	Buff_two_length = strlen((char *)Buff_two);
+
+	//Check for both the buffer size is same or not
+	if(Buff_one_length != Buff_two_length)
+	{
+		cout<<"Both the Buffers are Not Same size"<<endl;
+		return WRONG_SIZE;
+	}
+
+	//Compare the Buffer
+	while(Buff_one_length)
+	{
+		if(*Buff_one != *Buff_two)
+			return NOT_MATCHED;
+
+		Buff_one++;
+		Buff_two++;
+		Buff_one_length--;
+	}
+
+	return MATCHED;
+
+}
+
+//Compare the Two given Files with Binary Mode
+int UnitTest::CompareFiles(char *file1, char *file2)
+{
+	fstream oldf;
+	fstream newf;
+
+	unsigned char *oldb, *newb;
+	fstream::pos_type olength, nlength;
+
+	oldf.open(file1, ios::in);
+	if(oldf.is_open())
+	{
+		oldf.seekg(0, ios::end);
+		olength = oldf.tellg();
+		oldf.seekg(0, ios::beg);
+		oldb = new unsigned char[olength];
+		oldf.read((char *)oldb, olength);
+		if(oldf.bad())
+			return FILE_READ_ERROR;
+
+		oldf.close();
+	}
+	else
+	{
+		oldf.close();
+		return FILE_OPEN_ERROR;
+	}
+
+
+	newf.open(file2, ios::in);
+	if(newf.is_open())
+	{
+		newf.seekp(0, ios::end);
+		nlength = newf.tellg();
+		newf.seekg(0, ios::beg);
+		newb = new unsigned char[nlength];
+		newf.read((char *)newb, nlength);
+		if(newf.bad())
+			return FILE_READ_ERROR;
+
+		newf.close();
+	}
+	else
+	{
+		newf.close();
+		return FILE_OPEN_ERROR;
+	}
+	
+	//Compare the Two files output
+	return CompareBuffer(oldb, newb);
+
+}
+
+//Write the Buffer To File with Append Mode
+int UnitTest::DumpBuffer(unsigned char *Buffer, char *Filename)
+{
+	fstream   fhandle;
+	fhandle.open( Filename, ios::binary | ios::app | ios::out );
+
+	if( fhandle.is_open() )
+    {
+		if(strlen((char *)Buffer) == 0)
+			return WRONG_BUFFER;
+
+		fhandle.write((char *)Buffer, strlen((char *)Buffer));
+		if(fhandle.bad())
+		{
+			fhandle.close();
+			return FILEWRITE_ERROR;
+		}
+		fhandle.close();
+	}
+	else
+		return FILE_OPEN_ERROR;
+	 
+	return 0;
+
+}
+
+//Compare two YUV Output Files
+int UnitTest::CompareYUVOutputFile(char *file1, char *file2)
+{
+	fstream oldf;
+	fstream newf;
+
+	unsigned char *oldb, *newb;
+	fstream::pos_type olength, nlength;
+
+	oldf.open(file1, ios::in);
+	if(oldf.is_open())
+	{
+		oldf.seekg(0, ios::end);
+		olength = oldf.tellg();
+		oldf.seekg(0, ios::beg);
+		oldb = new unsigned char[olength];
+		oldf.read((char *)oldb, olength);
+		if(oldf.bad())
+			return FILE_READ_ERROR;
+
+		oldf.close();
+	}
+	else
+	{
+		oldf.close();
+		return FILE_OPEN_ERROR;
+	}
+
+
+	newf.open(file2, ios::in);
+	if(newf.is_open())
+	{
+		newf.seekp(0, ios::end);
+		nlength = newf.tellg();
+		newf.seekg(0, ios::beg);
+		newb = new unsigned char[nlength];
+		newf.read((char *)newb, nlength);
+		if(newf.bad())
+			return FILE_READ_ERROR;
+
+		newf.close();
+	}
+	else
+	{
+		newf.close();
+		return FILE_OPEN_ERROR;
+	}
+	
+	return CompareYUVBuffer(oldb, newb);
+}
+int UnitTest::CompareYUVBuffer(unsigned char *Buff_old, unsigned char *Buff_new)
+{
+
+	unsigned int offset =0;
+	unsigned int a_size, b_size;
+	unsigned int y_plane_area, u_plane_area, v_plane_area;
+
+	unsigned int width = 416;
+	unsigned int height = 240;
+	char plane;
+
+	unsigned int pixel, offs;
+	double pixel_x, pixel_y, frame, macroblock;
+	
+	a_size = strlen((char *)Buff_old);
+	b_size = strlen((char *)Buff_new);
+
+	if(a_size != b_size)
+		return NOT_MATCHED;
+
+	unsigned int i = 0;
+	while(i <= a_size)
+	{
+		if(Buff_old[i] != Buff_new[i])
+		{
+			offs =  i;
+		    y_plane_area = width * height;
+            u_plane_area = y_plane_area + y_plane_area * 0.25;
+            v_plane_area = u_plane_area + y_plane_area * 0.25;
+
+			pixel = offs % v_plane_area;
+			frame = offs / v_plane_area;
+
+			if(pixel < y_plane_area)
+			{
+				plane = 'Y';
+				pixel_x = pixel % width;
+                pixel_y = pixel / width;
+				macroblock = (ceil(pixel_x / 16.0), ceil(pixel_y / 16.0));
+			}
+			else if(pixel < u_plane_area)
+			{
+				plane = 'U';
+				pixel -= y_plane_area;
+				pixel_x = pixel % width;
+				pixel_y = pixel / width;
+				macroblock = (ceil(pixel_x / 8.0), ceil(pixel_y / 8.0));
+			}
+			else
+			{
+				plane = 'V';
+				pixel -= u_plane_area;
+				pixel_x = pixel % width;
+                pixel_y = pixel / width;
+				macroblock = (ceil(pixel_x / 8.0), ceil(pixel_y / 8.0));
+			}
+
+			printf("New File Differs from Referance file at frame - %d, macroblock - %f on the %c plane (offset %d)", frame,macroblock, plane, offs);
+			return NOT_MATCHED;
+		}
+		else
+			i++;
+	}
+	
+	return 0;
+}
+
+
---
+++ source/test/UnitTest.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <fstream>
+
+//Error Codes
+#define WRONG_SIZE		-1
+#define WRONG_BUFFER	-2
+#define NOT_MATCHED		1
+#define MATCHED			0
+#define FILE_OPEN_ERROR -3
+#define FILE_READ_ERROR -4
+#define FILEWRITE_ERROR -5
+
+
+using namespace std;
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+//UnitTest Class
+class UnitTest
+{
+private:
+  fstream   fhandle;                                      ///< file handle
+  int fSize;
+    
+public:
+  UnitTest()   {}
+ ~UnitTest()   {}
+   
+  static int CompareBuffer (unsigned char * Buff_one, unsigned char * Buff_two);
+  static int CompareFiles(char *file1, char *file2);
+  static int DumpBuffer(unsigned char *Buffer, char *Filename);
+  static int CompareYUVOutputFile(char *file1, char *file2);
+  static int CompareYUVBuffer(unsigned char *Buff_old, unsigned char *Buff_new);
+       
+};
