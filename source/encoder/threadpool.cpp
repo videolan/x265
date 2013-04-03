772272f1f67e985d5c610389e7e3394400dc4616
Steve Borho <steve@borho.org>
2013-04-03 08:59:27 -0500

    threadpool: use InterlockedCompareExchange64

--- source/encoder/threadpool.cpp
+++ source/encoder/threadpool.cpp
@@ -63,7 +63,7 @@ inline int __lzcnt_2x32(uint64_t x64)
 #endif // if _WIN64
 
 #define ATOMIC_OR(ptr, mask)            InterlockedOr64((volatile LONG64*)ptr, mask)
-#define ATOMIC_CAS(ptr, oldval, newval) InterlockedCompareExchange(ptr, newval, oldval)
+#define ATOMIC_CAS(ptr, oldval, newval) (uint64_t)InterlockedCompareExchange64((volatile LONG64*)ptr, newval, oldval)
 #define GIVE_UP_TIME()                  Sleep(0)
 
 #endif // ifdef __GNUC__
