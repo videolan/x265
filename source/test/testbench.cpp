3955089455a00ce087e66d2a0e39345ab9b56497
ggopu <devnull@localhost>
2013-04-08 03:03:55 -0700

    Implemented the Cycle testing

--- source/test/testbench.cpp
+++ source/test/testbench.cpp
@@ -23,7 +23,8 @@
 
 #include "unittest.h"
 #include "primitives.h"
-
+#include <time.h>
+#include <iostream>
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/timeb.h>
@@ -33,19 +34,103 @@
 #include <string.h>
 #include <math.h>
 
+#ifndef _TIMEVAL_H
+#define _TIMEVAL_H
+
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <time.h>
+
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+#define EPOCHFILETIME (116444736000000000i64)
+#else
+#define EPOCHFILETIME (116444736000000000LL)
+#endif
+__inline int gettimeofday(struct timeval *tv,  struct timezone *tz)
+{
+    FILETIME        ft;
+    LARGE_INTEGER   li;
+    __int64         t;
+    static int      tzflag;
+
+    if (tv)
+    {
+        GetSystemTimeAsFileTime(&ft);
+        li.LowPart  = ft.dwLowDateTime;
+        li.HighPart = ft.dwHighDateTime;
+        t  = li.QuadPart;       /* In 100-nanosecond intervals */
+        t -= EPOCHFILETIME;     /* Offset to the Epoch time */
+        t /= 10;                /* In microseconds */
+        tv->tv_sec  = (long)(t / 1000000);
+        tv->tv_usec = (long)(t % 1000000);
+    }
+
+    return 0;
+}
+
+#else  /* _WIN32 */
+
+#include <sys/time.h>
+
+#endif /* _WIN32 */
+
+#endif /* _TIMEVAL_H */
+
 using namespace x265;
 
 /* pbuf1, pbuf2: initialized to random pixel data and shouldn't write into them. */
 pixel *pbuf1, *pbuf2;
 uint16_t do_singleprimitivecheck = 0;
-uint16_t curpar = 0;
-
+uint16_t curpar = 0, cycletest = 0, cycletest_primitive = 0;
 #define BENCH_ALIGNS 16
+
+//Initialise the Func Names for all the Pixel Comp
+static const char *FuncNames[NUM_PARTITIONS] = {
+"4x4", "8x4", "4x8", "8x8", "4x16", "16x4", "8x16", "16x8", "16x16", "4x32", "32x4", "8x32",
+"32x8", "16x32", "32x16", "32x32", "4x64", "64x4", "8x64", "64x8", "16x64", "64x16", "32x64", "64x32", "64x64"
+};
+
 #if HIGH_BIT_DEPTH
 #define PIXEL_MAX ((1 << 10) - 1)
 #else
 #define PIXEL_MAX ((1 << 8) - 1)
 #endif
+
+double timevaldiff(struct timeval *starttime, struct timeval *finishtime)
+{
+    double msec;
+
+    msec =  (finishtime->tv_sec - starttime->tv_sec) * 1000;
+    msec += (double)(finishtime->tv_usec - starttime->tv_usec) / 1000;
+    return msec;
+}
+
+void check_cycle_count(pixelcmp cprimitive, pixelcmp vectorprimitive)
+{
+    uint32_t j = 0;
+    uint32_t res;
+    struct timeval ts, te;
+
+    gettimeofday(&ts, NULL);
+    for (j = 0; j < 100000; j++)
+    {
+        res = vectorprimitive(pbuf1, 16, pbuf2, 16);
+    }
+
+    gettimeofday(&te, NULL);
+    printf("Vectorised Primitive Time taken : %f MilliSeconds for %d Iterations\n",  timevaldiff(&ts, &te), j);
+
+    gettimeofday(&ts, NULL);
+    for (j = 0; j < 100000; j++)
+    {
+        res = cprimitive(pbuf1, 16, pbuf2, 16);
+    }
+
+    gettimeofday(&te, NULL);
+    printf("C Primitive Time taken : %f MilliSeconds  for %d Iterations \n", timevaldiff(&ts, &te), j);
+}
 
 // test all implemented pixel comparison primitives
 static int check_pixelprimitives(const EncoderPrimitives& cprimitives, const EncoderPrimitives& vectorprimitives)
@@ -62,14 +147,20 @@ static int check_pixelprimitives(const EncoderPrimitives& cprimitives, const Enc
             {
                 int vres = vectorprimitives.satd[curpar](pbuf1 + j, 16, pbuf2, 16);
                 int cres = cprimitives.satd[curpar](pbuf1 + j, 16, pbuf2, 16);
+
                 if (vres != cres)
                 {
-                    printf("FAILED COMPARISON for SATD partition - %d \n", curpar);
+                    printf("Test Failed : satd[%s] Partition \n", FuncNames[curpar]);
                     return -1;
                 }
+
                 j += 16;
             }
+
+            printf("Test Passed : satd[%s] Partition \n", FuncNames[curpar]);
+            check_cycle_count(cprimitives.satd[curpar], vectorprimitives.satd[curpar]);
         }
+
         // if the satd is not available for vector no need to test
         if (vectorprimitives.sad[curpar])
         {
@@ -80,16 +171,21 @@ static int check_pixelprimitives(const EncoderPrimitives& cprimitives, const Enc
                 int cres = cprimitives.sad[curpar](pbuf1 + j, 16, pbuf2, 16);
                 if (vres != cres)
                 {
-                    printf("FAILED COMPARISON for SATD partition - %d \n", curpar);
+                    printf("Test Failed : sad[%s] Partition \n", FuncNames[curpar]);
                     return -1;
                 }
+
                 j += 16;
             }
+
+            printf("Test Passed : sad[%s] Partition \n", FuncNames[curpar]);
+            check_cycle_count(cprimitives.sad[curpar], vectorprimitives.sad[curpar]);
         }
 
         if (do_singleprimitivecheck == 1)
             return 0;
     }
+
     if (vectorprimitives.sa8d_8x8)
     {
         j = 0;
@@ -99,12 +195,17 @@ static int check_pixelprimitives(const EncoderPrimitives& cprimitives, const Enc
             int cres = cprimitives.sa8d_8x8(pbuf1 + j, 16, pbuf2, 16);
             if (vres != cres)
             {
-                printf("FAILED COMPARISON for sa8d_8x8\n");
+                printf("Test Failed : sa8d_8x8\n");
                 return -1;
             }
+
             j += 16;
         }
+
+        printf("Test Passed : sa8d \n");
+        check_cycle_count(cprimitives.sa8d_8x8, vectorprimitives.sa8d_8x8);
     }
+
     if (vectorprimitives.sa8d_16x16)
     {
         j = 0;
@@ -114,11 +215,15 @@ static int check_pixelprimitives(const EncoderPrimitives& cprimitives, const Enc
             int cres = cprimitives.sa8d_16x16(pbuf1 + j, 16, pbuf2, 16);
             if (vres != cres)
             {
-                printf("FAILED COMPARISON for sa8d_8x8\n");
+                printf("Test Failed : sa8d_8x8\n");
                 return -1;
             }
+
             j += 16;
         }
+
+        printf("Test Passed : sa8d_8x8 \n");
+        check_cycle_count(cprimitives.sa8d_16x16, vectorprimitives.sa8d_16x16);
     }
 
     return 0;
@@ -134,16 +239,16 @@ int main(int argc, char *argv[])
     int ret = 0;
     int cpuid = CpuIDDetect();
 
-    for (int i = 1; i < argc-1; i+=2)
+    for (int i = 1; i < argc - 1; i += 2)
     {
         if (!strcmp(argv[i], "--primitive"))
         {
             do_singleprimitivecheck = 1;
-            curpar = atoi(argv[i+1]);
+            curpar = atoi(argv[i + 1]);
         }
         else if (!strcmp(argv[i], "--cpuid"))
         {
-            cpuid = atoi(argv[i+1]);
+            cpuid = atoi(argv[i + 1]);
         }
     }
 
@@ -173,11 +278,13 @@ int main(int argc, char *argv[])
 #if ENABLE_VECTOR_PRIMITIVES
     Setup_Vector_Primitives(vecprim, cpuid);
     ret = check_all_funcs(cprim, vecprim);
+
     if (ret)
     {
         fprintf(stderr, "x265: at least vector primitive has failed. Go and fix that Right Now!\n");
         return -1;
     }
+
 #endif
 
 #if ENABLE_ASM_PRIMITIVES
@@ -188,6 +295,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "x265: at least assembly primitive has failed. Go and fix that Right Now!\n");
         return -1;
     }
+
 #endif
 
     fprintf(stderr, "x265: All tests passed Yeah :)\n");
