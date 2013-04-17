235514ede6902cfafabc81b2c5d4c1939aeb8322
Mandar Gurav <devnull@localhost>
2013-04-17 16:55:21 -0700

    Move to latest xhevc source

--- source/encoder/macroblock.cpp
+++ source/encoder/macroblock.cpp
@@ -26,6 +26,7 @@
 
 #include "primitives.h"
 #include "Lib/TLibCommon/CommonDef.h"
+#include "butterfly.h"
 #include <algorithm>
 
 /* Used for filter */
@@ -56,26 +57,45 @@ void CDECL inversedst(short *tmp, short *block, int shift)  // input tmp, output
     }
 }
 
-template<int N, bool isLast>
-void CDECL filter_nonvertical(const short *coeff,
-                              pixel *      src,
-                              int          srcStride,
-                              pixel *      dst,
-                              int          dstStride,
-                              int          block_width,
-                              int          block_height,
-                              short        maxVal,
-                              int          offset,
-                              int          shift)
+template<int N, bool isFirst, bool isLast>
+void CDECL filter_8_nonvertical(const short *coeff,
+                                pixel *      src,
+                                int          srcStride,
+                                pixel *      dst,
+                                int          dstStride,
+                                int          block_width,
+                                int          block_height,
+                                int          bitDepth)
 {
     int row, col;
     short c[8];
 
-    c[0] = coeff[0];
-    c[1] = coeff[1];
+    src -= (N / 2 - 1);
+
+    int offset;
+    short maxVal;
+    int headRoom = IF_INTERNAL_PREC - bitDepth;
+    int shift = IF_FILTER_PREC;
+
 #if _MSC_VER
 #pragma warning(disable: 4127) // conditional expression is constant
 #endif
+    if (isLast)
+    {
+        shift += (isFirst) ? 0 : headRoom;
+        offset = 1 << (shift - 1);
+        offset += (isFirst) ? 0 : IF_INTERNAL_OFFS << IF_FILTER_PREC;
+        maxVal = (1 << bitDepth) - 1;
+    }
+    else
+    {
+        shift -= (isFirst) ? headRoom : 0;
+        offset = (isFirst) ? -IF_INTERNAL_OFFS << shift : 0;
+        maxVal = 0;
+    }
+
+    c[0] = coeff[0];
+    c[1] = coeff[1];
     if (N >= 4)
     {
         c[2] = coeff[2];
@@ -137,6 +157,59 @@ void CDECL filter_nonvertical(const short *coeff,
 #pragma warning(default: 4127) // conditional expression is constant
 #endif
 }
+
+void CDECL partialButterfly16(short *src, short *dst, int shift, int line)
+{
+    int j, k;
+    int E[8], O[8];
+    int EE[4], EO[4];
+    int EEE[2], EEO[2];
+    int add = 1 << (shift - 1);
+
+    for (j = 0; j < line; j++)
+    {
+        /* E and O */
+        for (k = 0; k < 8; k++)
+        {
+            E[k] = src[k] + src[15 - k];
+            O[k] = src[k] - src[15 - k];
+        }
+
+        /* EE and EO */
+        for (k = 0; k < 4; k++)
+        {
+            EE[k] = E[k] + E[7 - k];
+            EO[k] = E[k] - E[7 - k];
+        }
+
+        /* EEE and EEO */
+        EEE[0] = EE[0] + EE[3];
+        EEO[0] = EE[0] - EE[3];
+        EEE[1] = EE[1] + EE[2];
+        EEO[1] = EE[1] - EE[2];
+
+        dst[0] = (short)((g_aiT16[0][0] * EEE[0] + g_aiT16[0][1] * EEE[1] + add) >> shift);
+        dst[8 * line] = (short)((g_aiT16[8][0] * EEE[0] + g_aiT16[8][1] * EEE[1] + add) >> shift);
+        dst[4 * line] = (short)((g_aiT16[4][0] * EEO[0] + g_aiT16[4][1] * EEO[1] + add) >> shift);
+        dst[12 * line] = (short)((g_aiT16[12][0] * EEO[0] + g_aiT16[12][1] * EEO[1] + add) >> shift);
+
+        for (k = 2; k < 16; k += 4)
+        {
+            dst[k * line] = (short)((g_aiT16[k][0] * EO[0] + g_aiT16[k][1] * EO[1] + g_aiT16[k][2] * EO[2] +
+                                     g_aiT16[k][3] * EO[3] + add) >> shift);
+        }
+
+        for (k = 1; k < 16; k += 2)
+        {
+            dst[k * line] =  (short)((g_aiT16[k][0] * O[0] + g_aiT16[k][1] * O[1] + g_aiT16[k][2] * O[2] + g_aiT16[k][3] * O[3] +
+                                      g_aiT16[k][4] * O[4] + g_aiT16[k][5] * O[5] + g_aiT16[k][6] * O[6] + g_aiT16[k][7] * O[7] +
+                                      add) >> shift);
+        }
+
+        src += 16;
+        dst++;
+    }
+}
 }
 
 namespace x265 {
@@ -146,10 +219,16 @@ void Setup_C_MacroblockPrimitives(EncoderPrimitives& p)
 {
     p.inversedst = inversedst;
 
-    p.filter[FILTER_H_4_0] = filter_nonvertical<4, 0>;
-    p.filter[FILTER_H_4_1] = filter_nonvertical<4, 1>;
+    p.filter[FILTER_H_4_0_0] = filter_8_nonvertical<4, 0, 0>;
+    p.filter[FILTER_H_4_0_1] = filter_8_nonvertical<4, 0, 1>;
+    p.filter[FILTER_H_4_1_0] = filter_8_nonvertical<4, 1, 0>;
+    p.filter[FILTER_H_4_1_1] = filter_8_nonvertical<4, 1, 1>;
 
-    p.filter[FILTER_H_8_0] = filter_nonvertical<8, 0>;
-    p.filter[FILTER_H_8_1] = filter_nonvertical<8, 1>;
+    p.filter[FILTER_H_8_0_0] = filter_8_nonvertical<8, 0, 0>;
+    p.filter[FILTER_H_8_0_1] = filter_8_nonvertical<8, 0, 1>;
+    p.filter[FILTER_H_8_1_0] = filter_8_nonvertical<8, 1, 0>;
+    p.filter[FILTER_H_8_1_1] = filter_8_nonvertical<8, 1, 1>;
+
+    p.partial_butterfly[BUTTERFLY_16] = partialButterfly16;
 }
 }
