20520ae57da497497980eab4e0c041138009f8ba
sumalatha <devnull@localhost>
2013-05-20 12:14:09 +0530

    modified the scripts files  to take the application's(.exe) from the relative path to the curent directory

--- source/tools/performanceProfiler/Profiler.bat
+++ source/tools/performanceProfiler/Profiler.bat
@@ -5,80 +5,93 @@ for /f "tokens=1,2 delims=." %%a in ("%testFile%") do (
 set filename=%%a
 set extension=%%b
 )
-echo %filename%
-echo %extension%
 
+::run the encoder 
+echo  >> ..\..\..\build\%buildVersion%\Release\results.txt
+echo Test Sequence File : %testFile% >> ..\..\..\build\%buildVersion%\Release\results.txt
 
-
-::run the encoder
-cd "%encoderApplicationPath%"
-echo Begin
-if exist x265-cli.exe (
-if "%extension%"=="y4m" (
-	x265-cli.exe -i "%fileCfgPath%\%testFile%" -c "%fileCfgPath%\encoder_I_15P.cfg" -f %frames% -b "%encoderApplicationPath%\str_%filename%.bin" > "%encoderApplicationPath%"\encodeLog_%filename%.txt
+if exist ..\..\..\build\"%buildVersion%"\Release\x265-cli.exe (
+	if "%extension%"=="y4m" (
+	..\..\..\build\%buildVersion%\Release\x265-cli.exe -i "%inputFilePath%\%testFile%" -c "..\..\..\cfg\encoder_I_15P.cfg" -f %frames% -b "..\..\..\build\%buildVersion%\Release\str_%filename%.bin" > ..\..\..\build\%buildVersion%\Release\encodeLog_%filename%.txt
+	)
 )
-
-if "%extension%"=="yuv" (
-	x265-cli.exe -c "%fileCfgPath%\%filename%.cfg" -c "%fileCfgPath%\encoder_I_15P.cfg" -f %frames% -b "%encoderApplicationPath%\str_%filename%.bin" > "%encoderApplicationPath%"\encodeLog_%filename%.txt
-)
-)
+if not exist ..\..\..\build\"%buildVersion%"\Release\x265-cli.exe (
+	echo x265-cli.exe not Found 
+	echo x265-cli.exe Not Found >> ..\..\..\build\%buildVersion%\Release\results.txt  
+	goto error1
+	)
 
 
 :: run the HM decoder
-if exist "%encoderApplicationPath%\str_%filename%.bin" (
-
-	cd "%DecoderApplicationPath%"
-	if exist TAppDecoder.exe (
-		TAppDecoder.exe -b "%encoderApplicationPath%\str_%filename%.bin" -o "%DecoderApplicationPath%\decode_%filename%.yuv" > "%DecoderApplicationPath%"\decodeLog_%filename%.txt
-	)
+if exist "..\..\..\build\%buildVersion%\Release\str_%filename%.bin" (
+	if exist "..\HM Decoder\TAppDecoder.exe"  (
+		"..\HM Decoder\TAppDecoder.exe" -b "..\..\..\build\%buildVersion%\Release\str_%filename%.bin" -o "..\..\..\build\%buildVersion%\Release\decode_%filename%.yuv" > ..\..\..\build\%buildVersion%\Release\decodeLog_%filename%.txt )
+	if not exist "..\HM Decoder\TAppDecoder.exe" (
+		echo Decoder.exe not Found 
+		echo Decoder.exe Not Found >> ..\..\..\build\%buildVersion%\Release\results.txt  
+		goto error1 )
+		
 )
-
-:: convert y4m to yuv for running dr_psnr
-if "%extension%"=="y4m" (
-	cd "%fileCfgPath%"
-	if exist "raw_%filename%.yuv" (
-		del "raw_%filename%.yuv"
+if not exist ..\..\..\build\%buildVersion%\Release\str_%filename%.bin (
+	echo str_%filename%.bin Not Found 
+	echo str_%filename%.bin Not Found  >> ..\..\..\build\%buildVersion%\Release\results.txt
+	goto error1
 	)
-
-	cd "%ffmpegPath%"
-	ffmpeg.exe -i "%fileCfgPath%\%testFile%" -f rawvideo "%fileCfgPath%\raw_%filename%.yuv"
+:: converting input y4m files to raw yuv files
+if "%extension%"=="y4m" (
+	if not exist "%ffmpegPath%\ffmpeg.exe" (
+		echo ffmpeg.exe not Found 
+		echo ffmpeg.exe Not Found >> ..\..\..\build\%buildVersion%\Release\results.txt  
+		goto error1
+	)
+	if not exist "%inputFilePath%\raw_%filename%.yuv" (
+	%ffmpegPath%\ffmpeg.exe -i "%inputFilePath%\%testFile%" -f rawvideo "%inputFilePath%\raw_%filename%.yuv"
+	)
 )
 
 
 :: run dr_psnr
-cd  "%psnrApplicationPath%"
-if exist dr_psnr.exe (
-	if exist "%DecoderApplicationPath%\decode_%filename%.yuv" (
-		if "%extension%"=="yuv" (
-			dr_psnr.exe -r "%fileCfgPath%\%filename%.yuv" -c "%DecoderApplicationPath%\decode_%filename%.yuv" -w 1280 -h 720 -e 10 > "%psnrApplicationPath%"\dr_psnr_output_%filename%.txt
-		)
+
+if exist ..\..\..\build\"%buildVersion%"\tools\dr_psnr\Release\dr_psnr.exe (
+	if exist ..\..\..\build\"%buildVersion%"\Release\decode_%filename%.yuv (
+		
 		if "%extension%"=="y4m" (
-			dr_psnr.exe -r "%fileCfgPath%\raw_%filename%.yuv" -c "%DecoderApplicationPath%\decode_%filename%.yuv" -w 1280 -h 720 -e 10 > "%psnrApplicationPath%"\dr_psnr_output_%filename%.txt
+			..\..\..\build\"%buildVersion%"\tools\dr_psnr\Release\dr_psnr.exe -r "%inputFilePath%\raw_%filename%.yuv" -c "..\..\..\build\%buildVersion%\Release\decode_%filename%.yuv" -w 1280 -h 720 -e %frames% > "..\..\..\build\%buildVersion%\Release\dr_psnr_output_%filename%.txt"
 		)
 	)
 )
+if not exist ..\..\..\build\"%buildVersion%"\tools\dr_psnr\Release\dr_psnr.exe (
+		echo PSNR.exe not Found 
+		echo PSNR.exe Not Found >> ..\..\..\build\%buildVersion%\Release\results.txt  
+		goto error1
+	)
 
 
-echo : >>"%encoderApplicationPath%"\results.txt
-echo Test Sequence File : %testFile% >> "%encoderApplicationPath%"\results.txt
-if exist "%psnrApplicationPath%"\dr_psnr_output_%filename%.txt (
-	findstr /l "Avg " "%psnrApplicationPath%"\dr_psnr_output_%filename%.txt > temp.txt
+if exist "..\..\..\build\%buildVersion%\Release\dr_psnr_output_%filename%.txt" (
+	findstr /l "Avg " "..\..\..\build\%buildVersion%\Release\dr_psnr_output_%filename%.txt" > temp.txt
 	for /f "tokens=2,3,4" %%a in (temp.txt) do (
-		echo PSNR Y  : %%a  >> "%encoderApplicationPath%"\results.txt
-		echo PSNR U  : %%b  >> "%encoderApplicationPath%"\results.txt
-		echo PSNR V  : %%c  >> "%encoderApplicationPath%"\results.txt
+		echo PSNR Y  : %%a  >> "..\..\..\build\%buildVersion%\Release\results.txt"
+		echo PSNR U  : %%b  >> "..\..\..\build\%buildVersion%\Release\results.txt"
+		echo PSNR V  : %%c  >> "..\..\..\build\%buildVersion%\Release\results.txt"
 		)
 	)
 
 
 		
 	
-cd "%encoderApplicationPath%"
-if exist "%encoderApplicationPath%"\encodeLog_%filename%.txt (
 
-	findstr /b "Bytes written to file: " "%encoderApplicationPath%"\encodeLog_%filename%.txt >> "%encoderApplicationPath%"\results.txt
+if exist ..\..\..\build\%buildVersion%\Release\encodeLog_%filename%.txt (
+
+	findstr /b "Bytes written to file: " ..\..\..\build\%buildVersion%\Release\encodeLog_%filename%.txt >> ..\..\..\build\%buildVersion%\Release\results.txt
 	
-    findstr /l "sec"  "%encoderApplicationPath%"\encodeLog_%filename%.txt >> "%encoderApplicationPath%"\results.txt
+    findstr /l "sec"  ..\..\..\build\%buildVersion%\Release\encodeLog_%filename%.txt >> ..\..\..\build\%buildVersion%\Release\results.txt
+	
+goto normalExit
 	
 )
 
+:error1
+echo Aborting operation.. >> ..\..\..\build\%buildVersion%\Release\results.txt
+
+:normalExit
+echo Finished processing %testFile%
