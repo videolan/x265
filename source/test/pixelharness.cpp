9bc0e4b4b63c2dcc1d31d54a48bfdb62933e756a
Steve Borho <steve@borho.org>
2013-06-05 14:48:29 -0500

    Backed out changeset: ce41a92cec25

--- source/test/pixelharness.cpp
+++ source/test/pixelharness.cpp
@@ -270,7 +270,7 @@ bool PixelHarness::check_block_copy_p_s(x265::blockcpy_p_s ref, x265::blockcpy_p
 
 bool PixelHarness::testCorrectness(const EncoderPrimitives& ref, const EncoderPrimitives& opt)
 {
-    for (uint16_t curpar = 0; curpar < NUM_PARTITIONS; curpar++)
+    for (uint16_t curpar = 0; curpar < 8; curpar++)
     {
         if (opt.satd[curpar])
         {
