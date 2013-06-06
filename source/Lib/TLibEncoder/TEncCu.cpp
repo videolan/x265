83d00b0cbff13d385c75763de84f9c0ed1523507
Deepthi <devnull@localhost>
2013-06-06 18:54:36 +0530

    Modeling SATD distortion

--- source/Lib/TLibEncoder/TEncCu.cpp
+++ source/Lib/TLibEncoder/TEncCu.cpp
@@ -1781,7 +1781,7 @@ Void TEncCu::xCheckRDCostMerge2Nx2N(TComDataCU*& rpcBestCU, TComDataCU*& rpcTemp
                     rpcTempCU->setSkipFlagSubParts(rpcTempCU->getQtRootCbf(0) == 0, 0, uhDepth);
                     Int orgQP = rpcTempCU->getQP(0);
                     xCheckDQP(rpcTempCU);
-                    xCheckBestMode(rpcBestCU, rpcTempCU, uhDepth, true);
+                    xCheckBestMode(rpcBestCU, rpcTempCU, uhDepth);
                     rpcTempCU->initEstData(uhDepth, orgQP);
 
                     if (m_pcEncCfg->getUseFastDecisionForMerge() && !bestIsSkip)
@@ -1844,20 +1844,24 @@ Void TEncCu::xCheckRDCostInter(TComDataCU*& rpcBestCU, TComDataCU*& rpcTempCU, P
         return;
     }
 
+    UInt partEnum = PartitionFromSizes(rpcTempCU->getWidth(0), rpcTempCU->getHeight(0));
     if (m_pcEncCfg->getUseRateCtrl() && m_pcEncCfg->getLCULevelRC() && ePartSize == SIZE_2Nx2N && uhDepth <= m_addSADDepth)
     {
         /* TODO: this needs tobe tested with RC enable, currently RC enable x265 is not working */
-        UInt partEnum = PartitionFromSizes(rpcTempCU->getWidth(0), rpcTempCU->getHeight(0));
+    
         UInt SAD = primitives.sad[partEnum]( (pixel *)m_ppcOrigYuv[uhDepth]->getLumaAddr(), m_ppcOrigYuv[uhDepth]->getStride(),
                                         (pixel *)m_ppcPredYuvTemp[uhDepth]->getLumaAddr(), m_ppcPredYuvTemp[uhDepth]->getStride());
         m_temporalSAD = (Int)SAD;
     }
 
-#if !FAST_MODE_DECISION
     m_pcPredSearch->encodeResAndCalcRdInterCU(rpcTempCU, m_ppcOrigYuv[uhDepth], m_ppcPredYuvTemp[uhDepth], m_ppcResiYuvTemp[uhDepth], m_ppcResiYuvBest[uhDepth], m_ppcRecoYuvTemp[uhDepth], false);
-    rpcTempCU->getTotalCost()  = CALCRDCOST(rpcTempCU->getTotalBits(), rpcTempCU->getTotalDistortion(), m_pcRdCost->m_dLambda);
+#if FAST_MODE_DECISION
+    UInt SATD = primitives.satd[partEnum]( (pixel *)m_ppcOrigYuv[uhDepth]->getLumaAddr(), m_ppcOrigYuv[uhDepth]->getStride(),
+                                        (pixel *)m_ppcPredYuvTemp[uhDepth]->getLumaAddr(), m_ppcPredYuvTemp[uhDepth]->getStride());
+    rpcTempCU->getTotalDistortion() = SATD;
 #endif
-
+    rpcTempCU->getTotalCost()  = CALCRDCOST(rpcTempCU->getTotalBits(), rpcTempCU->getTotalDistortion(), m_pcRdCost->m_dLambda);
+    
     xCheckDQP(rpcTempCU);
 
     xCheckBestMode(rpcBestCU, rpcTempCU, uhDepth);
@@ -2051,15 +2055,10 @@ Void TEncCu::xCheckIntraPCM(TComDataCU*& rpcBestCU, TComDataCU*& rpcTempCU)
  * \param rpcTempCU
  * \returns Void
  */
-Void TEncCu::xCheckBestMode(TComDataCU*& rpcBestCU, TComDataCU*& rpcTempCU, UInt uiDepth, bool isMerge)
+Void TEncCu::xCheckBestMode(TComDataCU*& rpcBestCU, TComDataCU*& rpcTempCU, UInt uiDepth)
 {
     if (rpcTempCU->getTotalCost() < rpcBestCU->getTotalCost())
     {
-#if FAST_MODE_DECISION
-        if(!isMerge)
-            m_pcPredSearch->encodeResAndCalcRdInterCU(rpcTempCU, m_ppcOrigYuv[uiDepth], m_ppcPredYuvTemp[uiDepth], m_ppcResiYuvTemp[uiDepth], m_ppcResiYuvBest[uiDepth], m_ppcRecoYuvTemp[uiDepth], false);
-#endif
-
         TComYuv* pcYuv;
         // Change Information data
         TComDataCU* pcCU = rpcBestCU;
