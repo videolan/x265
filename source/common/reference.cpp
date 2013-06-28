50af0f6f5f6fa122f200b2c7c17cb38bd984971d
Steve Borho <steve@borho.org>
2013-06-28 13:21:37 -0500

    reference: remove obsolete code

--- source/common/reference.cpp
+++ source/common/reference.cpp
@@ -164,10 +164,4 @@ void MotionReference::generateReferencePlane(int x)
     pixel *dstPtr3 = m_lumaPlane[x][3] - s_tmpMarginY * m_lumaStride - s_tmpMarginX;
 
     primitives.filterVmulti(g_bitDepthY, intPtr, m_intStride, dstPtr1, dstPtr2, dstPtr3, m_lumaStride, m_filterWidth, m_filterHeight, m_reconPic->m_iLumaMarginX - s_tmpMarginX, m_reconPic->m_iLumaMarginY - s_tmpMarginY);
-    //if (x > 0)
-    //{
-    //    /* the Y=0 planes were not extended by the horizontal filter */
-    //    int bufOffset = -(s_tmpMarginY + s_intMarginY) * m_lumaStride - (s_tmpMarginX + s_intMarginX);
-    //    m_reconPic->xExtendPicCompBorder((Pel *)m_lumaPlane[x][0] + bufOffset, m_lumaStride, m_filterWidth + (2 * s_intMarginX), m_filterHeight + (2 * s_intMarginY), m_reconPic->m_iLumaMarginX - s_tmpMarginX - s_intMarginX, m_reconPic->m_iLumaMarginY - s_tmpMarginY - s_intMarginY);
-    //}
 }
