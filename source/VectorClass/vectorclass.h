a5bda8ead90bbe5aff25fffdcee3e2fe504e3029
Steve Borho <steve@borho.org>
2013-07-12 14:40:06 -0500

    remove vector class floating point, simplify includes

--- source/VectorClass/vectorclass.h
+++ source/VectorClass/vectorclass.h
@@ -28,27 +28,18 @@
 #ifndef VECTORCLASS_H
 #define VECTORCLASS_H  102
 
-
 #include "instrset.h"        // Select supported instruction set
 
 #if INSTRSET < 2             // SSE2 required
   #error Please compile for the SSE2 instruction set or higher
-#else
+#endif
 
 #include "vectori128.h"      // 128-bit integer vectors
-//#include "vectorf128.h"      // 128-bit floating point vectors
+
 #if INSTRSET >= 8
   #include "vectori256.h"    // 256-bit integer vectors, requires AVX2 instruction set
 #else
   #include "vectori256e.h"   // 256-bit integer vectors, emulated
-#endif  // INSTRSET >= 8
-#if INSTRSET >= 7
-//  #include "vectorf256.h"    // 256-bit floating point vectors, requires AVX instruction set
-#else
-//  #include "vectorf256e.h"   // 256-bit floating point vectors, emulated
-#endif  // INSTRSET >= 7
-
-#endif  // INSTRSET < 2
-
+#endif
 
 #endif  // VECTORCLASS_H
