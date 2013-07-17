442c59306d221ce9a88c28434c6c88bc0d0f888c
ggopu <devnull@localhost>
2013-07-17 11:33:20 +0530

    TShortYUV : Implemented perfomance Primitives pixelsub_sp

--- source/common/primitives.h
+++ source/common/primitives.h
@@ -215,6 +215,7 @@ typedef void (*filterHmulti_t)(int bitDepth, pixel *src, int srcStride, short *m
 typedef void (*dequant_t)(int bitDepth, const int* src, int* dst, int width, int height, int mcqp_miper, int mcqp_mirem, bool useScalingList, unsigned int trSizeLog2, int *dequantCoef);
 typedef uint32_t (*quant_t)(int *coef, int *quantCoeff, int *deltaU, int *qCoef, int qBits, int add, int numCoeff);
 typedef void (*weightpUni_t)(short *src, pixel *dst, int srcStride, int dstStride, int width, int height, int w0, int round, int shift, int offset, int bitDepth);
+typedef void (*pixelsub_sp_t)(int bx, int by, short *dst, intptr_t dstride, pixel *src0, pixel *src1, intptr_t sstride0, intptr_t sstride1); 
 
 /* Define a structure containing function pointers to optimized encoder
  * primitives.  Each pointer can reference either an assembly routine,
@@ -265,6 +266,7 @@ struct EncoderPrimitives
     transpose_t     transpose[NUM_SQUARE_BLOCKS];
 
     weightpUni_t    weightpUni;
+    pixelsub_sp_t   pixelsubsp;
 };
 
 /* This copy of the table is what gets used by the encoder.
