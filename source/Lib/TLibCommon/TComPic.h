c8d89271c03659c05dfc011368ad29971a708965
Steve Borho <steve@borho.org>
2013-08-08 13:35:47 -0500

    TComPic: nit

--- source/Lib/TLibCommon/TComPic.h
+++ source/Lib/TLibCommon/TComPic.h
@@ -70,7 +70,7 @@ private:
 
 public:
 
-    volatile uint32_t     *m_complete_enc;       // Array of Col number that was finished stage encode
+    volatile uint32_t    *m_complete_enc;       // Array of Col number that was finished stage encode
     x265::LookaheadFrame  m_lowres;
 
     TComPic();
