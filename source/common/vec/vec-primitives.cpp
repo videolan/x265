be27691fa5baed4236c8bb342e307acd9081ae2d
Steve Borho <steve@borho.org>
2013-09-06 15:20:21 -0500

    vec: simplify externs, fix MSVC compiler version dependencies

--- source/common/vec/vec-primitives.cpp
+++ source/common/vec/vec-primitives.cpp
@@ -28,40 +28,19 @@ bool hasXOP(void); // instr_detect.cpp
 namespace x265 {
 // private x265 namespace
 
-/* These functions are defined by C++ files in this folder. Depending on your
- * compiler, some of them may be undefined.  The #if logic here must match the
- * file lists in CMakeLists.txt */
-#if defined(__GNUC__) || defined(_MSC_VER)
 extern void Setup_Vec_Primitives_sse3(EncoderPrimitives&);
 extern void Setup_Vec_Primitives_ssse3(EncoderPrimitives&);
 extern void Setup_Vec_Primitives_sse41(EncoderPrimitives&);
-#endif
-
-#if defined(__INTEL_COMPILER)
-extern void Setup_Vec_Primitives_avx(EncoderPrimitives&);
-extern void Setup_Vec_Primitives_avx2(EncoderPrimitives&);
-
-#elif defined(__GNUC__)
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
-extern void Setup_Vec_Primitives_avx(EncoderPrimitives&);
-#endif
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 7
-extern void Setup_Vec_Primitives_avx2(EncoderPrimitives&);
-#endif
-
-#elif defined(_MSC_VER)
-#if _MSC_VER >= 1600  // VC10
 extern void Setup_Vec_Primitives_avx(EncoderPrimitives&);
 extern void Setup_Vec_Primitives_xop(EncoderPrimitives&);
-#endif
-#if _MSC_VER >= 1700  // VC11
 extern void Setup_Vec_Primitives_avx2(EncoderPrimitives&);
-#endif
-#endif
 
 /* Use primitives for the best available vector architecture */
 void Setup_Vector_Primitives(EncoderPrimitives &p, int cpuid)
 {
+    /* These functions are defined by C++ files in this folder. Depending on your
+     * compiler, some of them may be undefined.  The #if logic here must match the
+     * file lists in CMakeLists.txt */
 #if defined(__INTEL_COMPILER)
     if (cpuid > 2) Setup_Vec_Primitives_sse3(p);
     if (cpuid > 3) Setup_Vec_Primitives_ssse3(p);
@@ -88,10 +67,8 @@ void Setup_Vector_Primitives(EncoderPrimitives &p, int cpuid)
     if (cpuid > 3) Setup_Vec_Primitives_ssse3(p);
     if (cpuid > 4) Setup_Vec_Primitives_sse41(p);
 
-#if _MSC_VER >= 1700 // VC10
-    if (cpuid > 6) hasXOP() ? Setup_Vec_Primitives_xop(p) : Setup_Vec_Primitives_avx(p);
-#endif
 #if _MSC_VER >= 1700 // VC11
+    if (cpuid > 6) hasXOP() ? Setup_Vec_Primitives_xop(p) : Setup_Vec_Primitives_avx(p);
     if (cpuid > 7) Setup_Vec_Primitives_avx2(p);
 #endif
 
