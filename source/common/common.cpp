814b2182e4fab966a432926075f502095e83404b
Steve Borho <steve@borho.org>
2013-09-19 19:51:55 -0500

    common: improve logging string for lookahead configuration - conveys more info

--- source/common/common.cpp
+++ source/common/common.cpp
@@ -392,7 +392,7 @@ void x265_print_params(x265_param_t *param)
     {
         x265_log(param, X265_LOG_INFO, "RDpenalty                    : %d\n", param->rdPenalty);
     }
-    x265_log(param, X265_LOG_INFO, "Lookahead len / -b / bAdapt  : %d / %d / %d\n", param->lookaheadDepth, param->bframes, param->bFrameAdaptive);
+    x265_log(param, X265_LOG_INFO, "Lookahead / bframes / badapt : %d / %d / %d\n", param->lookaheadDepth, param->bframes, param->bFrameAdaptive);
     x265_log(param, X265_LOG_INFO, "tools: ");
 #define TOOLOPT(FLAG, STR) if (FLAG) fprintf(stderr, "%s ", STR)
     TOOLOPT(param->bEnableRectInter, "rect");
