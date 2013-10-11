f22dcd20810c066def85a00c920427cf4af56247
Dnyaneshwar Gorade <dnyaneshwar@multicorewareinc.com>
2013-10-11 16:08:52 +0530

    intra-sse3.cpp: Replace PredIntraAng4_m_26 vector class function with intrinsic.

--- source/common/vec/intra-sse3.cpp
+++ source/common/vec/intra-sse3.cpp
@@ -1861,37 +1861,41 @@ void PredIntraAng4_m_21(pixel* dst, int dstStride, pixel *refMain, int dirMode)
 
 void PredIntraAng4_m_26(pixel* dst, int dstStride, pixel *refMain, int dirMode)
 {
-    PRED_INTRA_ANG4_START
+    PRED_INTRA_ANGLE_4_START
 
-        tmp16_1 = (Vec16uc)load_partial(const_int(8), refMain - 3);
+    tmp16_1 = _mm_loadl_epi64((__m128i*)(refMain - 3));
+    row41 = _mm_unpacklo_epi8(tmp16_1, _mm_setzero_si128());
 
-    row41 = extend_low(tmp16_1);    //offsets(-4,-3,-2,-1)
-    tmp2uq = reinterpret_i(tmp16_1);
-    tmp2uq = tmp2uq >> 8;
-    tmp16_2 = reinterpret_i(tmp2uq);
-    row42 = extend_low(tmp16_2);    //offsets(-3,-2,-1,0)
+    tmp2 = tmp16_1;
+    tmp2 = _mm_srl_epi64(tmp2,_mm_cvtsi32_si128(8));
 
-    row31 = row42;                  //offsets(-3,-2,-1,0)
-    tmp2uq = reinterpret_i(tmp16_1);
-    tmp2uq = tmp2uq >> 16;
-    tmp16_2 = reinterpret_i(tmp2uq);
-    row32 = extend_low(tmp16_2);    //offsets(-2,-1,0,1)
+    tmp16_2 = tmp2;
+    row42 = _mm_unpacklo_epi8(tmp16_2, _mm_setzero_si128());
 
-    row21 = row32;                  //offsets(-2,-1,0,1)
-    tmp2uq = reinterpret_i(tmp16_1);
-    tmp2uq = tmp2uq >> 24;
-    tmp16_2 = reinterpret_i(tmp2uq);
-    row22 = extend_low(tmp16_2);    //offsets(-1,0,1,2)
+    row31 = row42;
+    tmp2 = tmp16_1;
+    tmp2 = _mm_srl_epi64(tmp2,_mm_cvtsi32_si128(16));
 
-    row11 = row22;                  //offsets(-1,0,1,2)
-    tmp2uq = reinterpret_i(tmp16_1);
-    tmp2uq = tmp2uq >> 32;
-    tmp16_2 = reinterpret_i(tmp2uq);
-    row12 = extend_low(tmp16_2);    //offsets(0,1,2,3)
+    tmp16_2 = tmp2;
+    row32 = _mm_unpacklo_epi8(tmp16_2, _mm_setzero_si128());
 
-    v_deltaPos = v_ipAngle = -26;
+    row21 = row32;
+    tmp2 = tmp16_1;
+    tmp2 = _mm_srl_epi64(tmp2,_mm_cvtsi32_si128(24));
 
-    PRED_INTRA_ANG4_END
+    tmp16_2 = tmp2;
+    row22 = _mm_unpacklo_epi8(tmp16_2, _mm_setzero_si128());
+
+    row11 = row22;
+    tmp2 = tmp16_1;
+    tmp2 = _mm_srl_epi64(tmp2,_mm_cvtsi32_si128(32));
+
+    tmp16_2 = tmp2;
+    row12 = _mm_unpacklo_epi8(tmp16_2, _mm_setzero_si128());
+
+    deltaPos = ipAngle = _mm_set1_epi16(-26);
+
+    PRED_INTRA_ANGLE_4_END
 }
 
 void PredIntraAng4_m_32(pixel* dst, int dstStride, pixel *refMain, int /*dirMode*/)
