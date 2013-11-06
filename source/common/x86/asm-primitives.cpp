afc9bbfb63dba93b7ef65703ab860f38b16bc011
Praveen Tiwari <devnull@localhost>
2013-11-06 20:47:52 +0530

    asm code for blockcopy_sp, 32x64

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -356,6 +356,7 @@ void Setup_Assembly_Primitives(EncoderPrimitives &p, int cpuMask)
         p.chroma_copy_sp[CHROMA_32x16] = x265_blockcopy_sp_32x16_sse2;
         p.chroma_copy_sp[CHROMA_32x24] = x265_blockcopy_sp_32x24_sse2;
         p.chroma_copy_sp[CHROMA_32x32] = x265_blockcopy_sp_32x32_sse2;
+        p.luma_copy_sp[LUMA_32x64] = x265_blockcopy_sp_32x64_sse2;
 #if X86_64
         p.satd[LUMA_8x32] = x265_pixel_satd_8x32_sse2;
         p.satd[LUMA_16x4] = x265_pixel_satd_16x4_sse2;
