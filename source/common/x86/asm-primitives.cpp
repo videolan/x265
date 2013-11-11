04ecccef8d03d847775b9e210f41e606a0ccb8c7
Praveen Tiwari <devnull@localhost>
2013-11-11 19:20:41 +0530

    asm code for blockcopy_ps_2x8

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -460,6 +460,7 @@ void Setup_Assembly_Primitives(EncoderPrimitives &p, int cpuMask)
         // review.
 
         p.chroma_copy_ps[CHROMA_2x4] = x265_blockcopy_ps_2x4_sse4;
+        p.chroma_copy_ps[CHROMA_2x8] = x265_blockcopy_ps_2x8_sse4;
         p.chroma_copy_ps[CHROMA_4x2] = x265_blockcopy_ps_4x2_sse4;
         p.chroma_copy_ps[CHROMA_4x4] = x265_blockcopy_ps_4x4_sse4;
         p.chroma_copy_ps[CHROMA_4x8] = x265_blockcopy_ps_4x8_sse4;
--- source/common/x86/blockcopy8.asm
+++ source/common/x86/blockcopy8.asm
@@ -1688,6 +1688,58 @@ movd       [r0 + r1],     m0
 
 RET
 
+
+;-----------------------------------------------------------------------------
+; void blockcopy_ps_2x8(int16_t *dest, intptr_t destStride, pixel *src, intptr_t srcStride);
+;-----------------------------------------------------------------------------
+INIT_XMM sse4
+cglobal blockcopy_ps_2x8, 4, 4, 1, dest, destStride, src, srcStride
+
+add        r1,            r1
+
+movd       m0,            [r2]
+pmovzxbw   m0,            m0
+movd       [r0],          m0
+
+movd       m0,            [r2 + r3]
+pmovzxbw   m0,            m0
+movd       [r0 + r1],     m0
+
+movd       m0,            [r2 + 2 * r3]
+pmovzxbw   m0,            m0
+movd       [r0 + 2 * r1], m0
+
+lea        r2,            [r2 + 2 * r3]
+lea        r0,            [r0 + 2 * r1]
+
+movd       m0,            [r2 + r3]
+pmovzxbw   m0,            m0
+movd       [r0 + r1],     m0
+
+movd       m0,            [r2 + 2 * r3]
+pmovzxbw   m0,            m0
+movd       [r0 + 2 * r1], m0
+
+lea        r2,            [r2 + 2 * r3]
+lea        r0,            [r0 + 2 * r1]
+
+movd       m0,            [r2 + r3]
+pmovzxbw   m0,            m0
+movd       [r0 + r1],     m0
+
+movd       m0,            [r2 + 2 * r3]
+pmovzxbw   m0,            m0
+movd       [r0 + 2 * r1], m0
+
+lea        r2,            [r2 + 2 * r3]
+lea        r0,            [r0 + 2 * r1]
+
+movd       m0,            [r2 + r3]
+pmovzxbw   m0,            m0
+movd       [r0 + r1],     m0
+
+RET
+
 ;-----------------------------------------------------------------------------
 ; void blockcopy_ps_4x2(int16_t *dest, intptr_t destStride, pixel *src, intptr_t srcStride);
 ;-----------------------------------------------------------------------------
