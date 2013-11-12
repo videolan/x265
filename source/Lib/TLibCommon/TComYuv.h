27140d8beaccf20e75fb38510e97ab22e9caaa4f
Steve Borho <steve@borho.org>
2013-11-11 19:43:33 -0600

    TComYuv: de-hungarian nits

--- source/Lib/TLibCommon/TComYuv.h
+++ source/Lib/TLibCommon/TComYuv.h
@@ -129,9 +129,9 @@ public:
     void    copyToPartChroma(TComYuv* dstPicYuv, uint32_t uiDstPartIdx);
 
     //  Copy the part of Big YUV buffer to other Small YUV buffer
-    void    copyPartToYuv(TComYuv* dstPicYuv, uint32_t uiSrcPartIdx);
-    void    copyPartToLuma(TComYuv* dstPicYuv, uint32_t uiSrcPartIdx);
-    void    copyPartToChroma(TComYuv* dstPicYuv, uint32_t uiSrcPartIdx);
+    void    copyPartToYuv(TComYuv* dstPicYuv, uint32_t srcPartIdx);
+    void    copyPartToLuma(TComYuv* dstPicYuv, uint32_t srcPartIdx);
+    void    copyPartToChroma(TComYuv* dstPicYuv, uint32_t srcPartIdx);
 
     //  Copy YUV partition buffer to other YUV partition buffer
     void    copyPartToPartYuv(TComYuv* dstPicYuv, uint32_t partIdx, uint32_t width, uint32_t height, bool bLuma = true, bool bChroma = true);
