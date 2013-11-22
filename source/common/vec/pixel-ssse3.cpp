01aaa035ae8a77f83aa98bb4b88cdc8602f31cb4
Steve Borho <steve@borho.org>
2013-11-22 12:42:14 -0600

    pixel-ssse3: remove scale1D_128to64, we have ASM

--- source/common/vec/pixel-ssse3.cpp
+++ source/common/vec/pixel-ssse3.cpp
@@ -30,39 +30,6 @@ using namespace x265;
 
 namespace {
 #if !HIGH_BIT_DEPTH
-void scale1D_128to64(pixel *dst, pixel *src, intptr_t /*stride*/)
-{
-    const __m128i mask = _mm_setr_epi32(0x06040200, 0x0E0C0A08, 0x07050301, 0x0F0D0B09);
-
-    __m128i T00 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[0 * 16]), mask);
-    __m128i T01 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[1 * 16]), mask);
-    __m128i T02 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[2 * 16]), mask);
-    __m128i T03 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[3 * 16]), mask);
-    __m128i T04 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[4 * 16]), mask);
-    __m128i T05 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[5 * 16]), mask);
-    __m128i T06 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[6 * 16]), mask);
-    __m128i T07 = _mm_shuffle_epi8(_mm_loadu_si128((__m128i*)&src[7 * 16]), mask);
-
-    __m128i T10 = _mm_unpacklo_epi64(T00, T01);
-    __m128i T11 = _mm_unpackhi_epi64(T00, T01);
-    __m128i T12 = _mm_unpacklo_epi64(T02, T03);
-    __m128i T13 = _mm_unpackhi_epi64(T02, T03);
-    __m128i T14 = _mm_unpacklo_epi64(T04, T05);
-    __m128i T15 = _mm_unpackhi_epi64(T04, T05);
-    __m128i T16 = _mm_unpacklo_epi64(T06, T07);
-    __m128i T17 = _mm_unpackhi_epi64(T06, T07);
-
-    __m128i T20 = _mm_avg_epu8(T10, T11);
-    __m128i T21 = _mm_avg_epu8(T12, T13);
-    __m128i T22 = _mm_avg_epu8(T14, T15);
-    __m128i T23 = _mm_avg_epu8(T16, T17);
-
-    _mm_storeu_si128((__m128i*)&dst[0], T20);
-    _mm_storeu_si128((__m128i*)&dst[16], T21);
-    _mm_storeu_si128((__m128i*)&dst[32], T22);
-    _mm_storeu_si128((__m128i*)&dst[48], T23);
-}
-
 void scale2D_64to32(pixel *dst, pixel *src, intptr_t stride)
 {
     int i;
@@ -105,9 +72,9 @@ void scale2D_64to32(pixel *dst, pixel *src, intptr_t stride)
 namespace x265 {
 void Setup_Vec_PixelPrimitives_ssse3(EncoderPrimitives &p)
 {
-    p.sad[0] = p.sad[0];
 #if !HIGH_BIT_DEPTH
-    p.scale1D_128to64 = scale1D_128to64;
+    p.scale2D_64to32 = p.scale2D_64to32;
+#else
     p.scale2D_64to32 = scale2D_64to32;
 #endif
 }
