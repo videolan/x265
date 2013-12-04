a04b56ca0c85924489d6ae86f13d638638271ffa
Steve Borho <steve@borho.org>
2013-12-04 12:47:16 -0600

    remove Agner Fog's vector classes and their last few users
    We'll take a step back for HIGH_BIT_DEPTH perf for a few days, but these
    functions are all expected to have assembly coverage soon.  This allows us to
    remove a few hacks from our cmake scripts as well.

--- source/common/vec/vec-primitives.cpp
+++ source/common/vec/vec-primitives.cpp
@@ -63,8 +63,6 @@ void Setup_Vec_IPredPrimitives_sse41(EncoderPrimitives&);
 void Setup_Vec_IPFilterPrimitives_ssse3(EncoderPrimitives&);
 void Setup_Vec_IPFilterPrimitives_sse41(EncoderPrimitives&);
 
-void Setup_Vec_Pixel16Primitives_sse41(EncoderPrimitives&);
-
 /* Use primitives for the best available vector architecture */
 void Setup_Vector_Primitives(EncoderPrimitives &p, int cpuMask)
 {
@@ -74,8 +72,6 @@ void Setup_Vector_Primitives(EncoderPrimitives &p, int cpuMask)
         Setup_Vec_DCTPrimitives_sse3(p);
         Setup_Vec_BlockCopyPrimitives_sse3(p);
     }
-#else
-    if (cpuMask) p.sad[0] = p.sad[0]; // prevent compiler warnings
 #endif
 #ifdef HAVE_SSSE3
     if (cpuMask & X265_CPU_SSSE3)
@@ -88,9 +84,6 @@ void Setup_Vector_Primitives(EncoderPrimitives &p, int cpuMask)
 #ifdef HAVE_SSE4
     if (cpuMask & X265_CPU_SSE4)
     {
-#if HIGH_BIT_DEPTH
-        Setup_Vec_Pixel16Primitives_sse41(p);
-#endif /* !HIGH_BIT_DEPTH */
         Setup_Vec_IPredPrimitives_sse41(p);
         Setup_Vec_IPFilterPrimitives_sse41(p);
         Setup_Vec_DCTPrimitives_sse41(p);
