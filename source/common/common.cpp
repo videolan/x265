3e983ea5b940d7d1b0c9effe89a4b15c96f43c04
Deepthi Nandakumar <deepthi@multicorewareinc.com>
2013-12-27 14:57:23 +0530

    aq, cutree: preset changes
    1. Disable AQ at ultrafast/superfast presets
    2. Disable CUTree at ultrafast/superfast/veryfast/faster presets (requires b-adapt to be non-zero).
    3. tune-psnr disables only AQ, CUtree stays enabled (at appropriate presets).

--- source/common/common.cpp
+++ source/common/common.cpp
@@ -291,6 +291,8 @@ int x265_param_default_preset(x265_param *param, const char *preset, const char 
             param->bEnableSignHiding = 0;
             param->bEnableWeightedPred = 0;
             param->maxNumReferences = 1;
+            param->rc.aqStrength = 0.0;
+            param->rc.cuTree = 0;
         }
         else if (!strcmp(preset, "superfast"))
         {
@@ -305,6 +307,8 @@ int x265_param_default_preset(x265_param *param, const char *preset, const char 
             param->bEnableCbfFastMode = 1;
             param->bEnableWeightedPred = 0;
             param->maxNumReferences = 1;
+            param->rc.aqStrength = 0.0;
+            param->rc.cuTree = 0;
         }
         else if (!strcmp(preset, "veryfast"))
         {
@@ -317,6 +321,7 @@ int x265_param_default_preset(x265_param *param, const char *preset, const char 
             param->bEnableEarlySkip = 1;
             param->bEnableCbfFastMode = 1;
             param->maxNumReferences = 1;
+            param->rc.cuTree = 0;
         }
         else if (!strcmp(preset, "faster"))
         {
@@ -327,6 +332,7 @@ int x265_param_default_preset(x265_param *param, const char *preset, const char 
             param->bEnableEarlySkip = 1;
             param->bEnableCbfFastMode = 1;
             param->maxNumReferences = 1;
+            param->rc.cuTree = 0;
         }
         else if (!strcmp(preset, "fast"))
         {
@@ -392,9 +398,7 @@ int x265_param_default_preset(x265_param *param, const char *preset, const char 
     {
         if (!strcmp(tune, "psnr"))
         {
-            param->rc.aqMode = X265_AQ_NONE;
-            param->rc.aqStrength = 0.0;
-            param->rc.cuTree = 0;
+            param->rc.aqStrength = 0.0;            
         }
         else if (!strcmp(tune, "ssim"))
         {
@@ -519,9 +523,14 @@ int x265_check_params(x265_param *param)
     if (param->rc.aqMode == 0 && param->rc.cuTree)
     {
         param->rc.aqMode = X265_AQ_VARIANCE;
-        param->rc.aqStrength = 0;
+        param->rc.aqStrength = 0.0;
     }
 
+    if(param->bFrameAdaptive == 0 && param->rc.cuTree)
+    {
+        x265_log(NULL, X265_LOG_WARNING, "cuTree disabled, requires lookahead to be enabled\n");
+        param->rc.cuTree = 0;
+    }
     return check_failed;
 }
 
