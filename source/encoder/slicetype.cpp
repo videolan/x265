ebf0cec935e8dfee9728f22ef69f4c8f32cf0615
Steve Borho <steve@borho.org>
2014-01-31 14:56:33 -0600

    slicetype: use explicit float type constant

--- source/encoder/slicetype.cpp
+++ source/encoder/slicetype.cpp
@@ -1283,7 +1283,7 @@ void CostEstimate::weightsAnalyse(Lowres **frames, int b, int p0)
     unsigned int minscore = 0, origscore = 1;
     int found = 0;
 
-    w.setFromWeightAndOffset((int)(guessScale * 128 + 0.5), 0);
+    w.setFromWeightAndOffset((int)(guessScale * 128 + 0.5f), 0);
     mindenom = w.log2WeightDenom;
     minscale = w.inputWeight;
 
