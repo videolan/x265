fa51a80ad0d5a342b9d7c3f480afcc78cdeab9fb
Steve Borho <steve@borho.org>
2014-02-04 16:06:55 -0600

    ipfilterharness: fix loop bounds, caused crashes in 16bpp

--- source/test/ipfilterharness.cpp
+++ source/test/ipfilterharness.cpp
@@ -66,7 +66,7 @@ IPFilterHarness::IPFilterHarness()
     }
 
     /*[0] --- Random values  */
-    for (size_t i = 0; i < ipf_t_size * sizeof(pixel); i++)
+    for (int i = 0; i < ipf_t_size; i++)
     {
         pixel_test_buff[0][i] = rand() & PIXEL_MAX;
     }
