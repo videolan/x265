00ad0e9a7fceeb560dae5313917a99504701eaa0
Steve Borho <steve@borho.org>
2014-03-03 14:59:21 -0600

    TEncEntropy: fix gcc warnings

--- source/Lib/TLibEncoder/TEncEntropy.cpp
+++ source/Lib/TLibEncoder/TEncEntropy.cpp
@@ -388,9 +388,11 @@ void TEncEntropy::xEncodeTransform(TComDataCU* cu, uint32_t offsetLuma, uint32_t
         else
         {
             int trWidth  = width >> cu->getHorzChromaShift();
-            int trHeight = height >> cu->getVertChromaShift();
+#if _DEBUG
             // TODO: 4:2:2
+            int trHeight = height >> cu->getVertChromaShift();
             assert(trWidth == trHeight);
+#endif
             if (cbfU)
             {
                 m_entropyCoderIf->codeCoeffNxN(cu, (cu->getCoeffCb() + offsetChroma), absPartIdx, trWidth, depth, TEXT_CHROMA_U);
@@ -621,11 +623,11 @@ void TEncEntropy::encodeCoeff(TComDataCU* cu, uint32_t absPartIdx, uint32_t dept
     xEncodeTransform(cu, lumaOffset, chromaOffset, absPartIdx, depth, width, height, 0, bCodeDQP);
 }
 
-void TEncEntropy::encodeCoeffNxN(TComDataCU* cu, TCoeff* coeff, uint32_t absPartIdx, uint32_t trWidth, uint32_t trHeight, uint32_t depth, TextType ttype)
+void TEncEntropy::encodeCoeffNxN(TComDataCU* cu, TCoeff* coeff, uint32_t absPartIdx, uint32_t trWidth, uint32_t, uint32_t depth, TextType ttype)
 {
     // This is for Transform unit processing. This may be used at mode selection stage for Inter.
     // TODO: 4:2:2
-    assert(trWidth == trHeight);
+    //assert(trWidth == trHeight);
     m_entropyCoderIf->codeCoeffNxN(cu, coeff, absPartIdx, trWidth, depth, ttype);
 }
 
