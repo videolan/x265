91e78a49811b043ac5090d4ebbf522dc1053db3c
Steve Borho <steve@borho.org>
2014-03-04 13:50:40 -0600

    cli: do not call x265_setup_primitives() from printVersion()
    It now leads to redundant console output and it was interfering with --asm
    command line arguent functionality

--- source/x265.cpp
+++ source/x265.cpp
@@ -276,7 +276,6 @@ void CLIOptions::printVersion(x265_param *param)
 {
     x265_log(param, X265_LOG_INFO, "HEVC encoder version %s\n", x265_version_str);
     x265_log(param, X265_LOG_INFO, "build info %s\n", x265_build_info_str);
-    x265_setup_primitives(param, -1);
 }
 
 void CLIOptions::showHelp(x265_param *param)
