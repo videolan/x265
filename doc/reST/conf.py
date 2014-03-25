809cdaf5432f11b4c3414b94babfeb2030595280
Steve Borho <steve@borho.org>
2014-03-25 12:54:41 -0500

    rest: add Makefile for generating HTML from reST, cleanup some nits

--- doc/reST/conf.py
+++ doc/reST/conf.py
@@ -4,17 +4,14 @@
 
 source_suffix = '.rst'
 
-#Name of the master file 
+# Name of the master file 
 master_doc = 'index'
 
 # General information about the project.
-#This is the title of the project which appears on the top of HTML Document as well as PDF Document
-project = u'x265 Documentation'
+project = u'x265'
 
-
-#This is the Copyright Information that will appear on the bottom of the document
+# This is the Copyright Information that will appear on the bottom of the document
 copyright = u'2014 MulticoreWare Inc'
-
 
 # -- Options for HTML output ---------------------------------------------------
 html_theme = "default"
