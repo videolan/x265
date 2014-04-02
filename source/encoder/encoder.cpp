8a9f1a76a1fa6cab0f1452fd21af3600aafc761b
Aarthi Thirumalai <devnull@localhost>
2014-04-02 17:19:38 +0530

    param: set aq strength to 0 in CQP

--- source/encoder/encoder.cpp
+++ source/encoder/encoder.cpp
@@ -1415,6 +1415,7 @@ void Encoder::configure(x265_param *p)
         p->rc.aqMode = X265_AQ_NONE;
         p->rc.bitrate = 0;
         p->rc.cuTree = 0;
+        p->rc.aqStrength = 0;
     }
 
     if (p->rc.aqMode == 0 && p->rc.cuTree)
@@ -1432,6 +1433,11 @@ void Encoder::configure(x265_param *p)
     if (p->rc.aqStrength == 0 && p->rc.cuTree == 0)
     {
         p->rc.aqMode = X265_AQ_NONE;
+    }
+
+    if (p->rc.aqMode = X265_AQ_NONE && p->rc.cuTree == 0)
+    {
+        p->rc.aqStrength = 0;
     }
 
     if (p->internalCsp != X265_CSP_I420)
