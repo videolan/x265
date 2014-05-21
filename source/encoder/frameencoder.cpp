be30cd7f5305a467228ad1f16c8b3e508e672da3
Steve Borho <steve@borho.org>
2014-05-21 11:03:23 -0500

    frameencoder: fixup comment following variable rename

--- source/encoder/frameencoder.cpp
+++ source/encoder/frameencoder.cpp
@@ -1308,8 +1308,7 @@ int FrameEncoder::calcQpForCu(uint32_t cuAddr, double baseQp)
     int block_y = (cuAddr / m_pic->getPicSym()->getFrameWidthInCU()) * noOfBlocks;
     int block_x = (cuAddr * noOfBlocks) - block_y * m_pic->getPicSym()->getFrameWidthInCU();
 
-    /* Use cuTree offsets in m_pic->m_lowres.qpOffset if cuTree enabled and
-     * frame is referenced, else use AQ offsets */
+    /* Use cuTree offsets if cuTree enabled and frame is referenced, else use AQ offsets */
     double *qpoffs = (m_isReferenced && m_cfg->param->rc.cuTree) ? m_pic->m_lowres.qpCuTreeOffset : m_pic->m_lowres.qpAqOffset;
 
     int cnt = 0, idx = 0;
