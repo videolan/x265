bfd635463fabe9b6ddd6ea222e7b7d555920331a
Steve Borho <steve@borho.org>
2014-05-23 11:21:16 -0500

    vc9: define UINT64_MAX in our hacked stdint.h

--- source/compat/msvc/stdint.h
+++ source/compat/msvc/stdint.h
@@ -1,6 +1,16 @@
 #pragma once
 
-/* a minimal set of C99 types for use with MSVC */
+#ifndef _MSC_VER
+#error "Use this header only with Microsoft Visual C++ compilers!"
+#endif
+
+#include <crtdefs.h> // for intptr_t
+#if !defined(UINT64_MAX)
+#include <limits.h>
+#define UINT64_MAX _UI64_MAX
+#endif
+
+/* a minimal set of C99 types for use with MSVC (VC9) */
 
 typedef signed char int8_t;
 typedef short int int16_t;
@@ -12,4 +22,3 @@ typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned __int64 uint64_t;
 
-#include <crtdefs.h> // for intptr_t
