5dc3a7d414a6fe8664c43c1ca6d1f8e6a811f83b
Gopu Govindaswamy <devnull@localhost>
2014-06-19 10:24:02 +0530

    dpb: destroy and delete m_reconPicYuv from picSymFreeList

--- source/encoder/dpb.cpp
+++ source/encoder/dpb.cpp
@@ -51,6 +51,8 @@ DPB::~DPB()
     {
         TComPicSym* next = m_picSymFreeList->m_freeListNext;
         m_picSymFreeList->destroy();
+        m_picSymFreeList->m_reconPicYuv->destroy();
+        delete m_picSymFreeList->m_reconPicYuv;
         delete m_picSymFreeList;
         m_picSymFreeList = next;
     }
