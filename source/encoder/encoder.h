d65b0ea5e63aaa4734bbecdccc64dbdd6af6ddb4
Steve Borho <steve@borho.org>
2014-07-12 01:17:36 -0500

    encoder: remove unused variables

--- source/encoder/encoder.h
+++ source/encoder/encoder.h
@@ -149,9 +149,6 @@ public:
 
     int                m_useScalingListId; ///< Using quantization matrix i.e. 0=off, 1=default.
 
-    bool               m_transquantBypassEnableFlag;   ///< transquant_bypass_enable_flag setting in PPS.
-    bool               m_cuTransquantBypassFlagValue;  ///< if transquant_bypass_enable_flag, the fixed value to use for the per-CU cu_transquant_bypass_flag.
-
     bool               m_neutralChromaIndicationFlag;
     bool               m_pocProportionalToTimingFlag;
     int                m_numTicksPocDiffOneMinus1;
