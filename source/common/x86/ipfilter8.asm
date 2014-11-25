a76161bebf2217b481fde869b9e7a9862a6df293
Divya Manivannan <divya@multicorewareinc.com>
2014-11-25 11:34:14 +0530

    asm: luma_vpp[32x16] in avx2: improve 3808c->2491c

--- source/common/x86/ipfilter8.asm
+++ source/common/x86/ipfilter8.asm
@@ -5562,6 +5562,33 @@ cglobal interp_8tap_vert_pp_%1x%2, 4, 12, 15
 FILTER_VER_LUMA_AVX2_32xN 32, 32
 FILTER_VER_LUMA_AVX2_32xN 32, 64
 
+INIT_YMM avx2
+%if ARCH_X86_64 == 1
+cglobal interp_8tap_vert_pp_32x16, 4, 10, 15
+    mov             r4d, r4m
+    shl             r4d, 7
+
+%ifdef PIC
+    lea             r5, [tab_LumaCoeffVer_32]
+    add             r5, r4
+%else
+    lea             r5, [tab_LumaCoeffVer_32 + r4]
+%endif
+
+    lea             r4, [r1 * 3]
+    sub             r0, r4
+    lea             r6, [r3 * 3]
+    mova            m14, [pw_512]
+    mov             r9d, 2
+.loopW:
+    PROCESS_LUMA_AVX2_W16_16R
+    add             r2, 16
+    add             r0, 16
+    dec             r9d
+    jnz             .loopW
+    RET
+%endif
+
 ;-------------------------------------------------------------------------------------------------------------
 ; void interp_8tap_vert_%3_%1x%2(pixel *src, intptr_t srcStride, pixel *dst, intptr_t dstStride, int coeffIdx)
 ;-------------------------------------------------------------------------------------------------------------
