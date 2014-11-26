7b8ef74b07022ef633e2b61eb22fd8237dc3e12b
Aasaipriya Chandran <aasaipriya@multicorewareinc.com>
2014-11-26 11:53:40 +0530

    Luma_hpp[48x64] avx2 asm code : improved 25053c->17882c

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -1839,7 +1839,7 @@ void Setup_Assembly_Primitives(EncoderPrimitives &p, int cpuMask)
         p.luma_hpp[LUMA_64x32] = x265_interp_8tap_horiz_pp_64x32_avx2;
         p.luma_hpp[LUMA_64x16] = x265_interp_8tap_horiz_pp_64x16_avx2;
 
-
+        p.luma_hpp[LUMA_48x64] = x265_interp_8tap_horiz_pp_48x64_avx2;
 
         p.luma_vpp[LUMA_4x4] = x265_interp_8tap_vert_pp_4x4_avx2;
         p.luma_vpp[LUMA_8x4] = x265_interp_8tap_vert_pp_8x4_avx2;
