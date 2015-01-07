4f8dda6263b1ad0e1f0a17c890192f71acb8aa61
Dnyaneshwar G <dnyaneshwar@multicorewareinc.com>
2015-01-07 18:40:30 +0530

    intrapred: updated asm code for all_ang_pred_32x32

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -1748,11 +1748,10 @@ void Setup_Assembly_Primitives(EncoderPrimitives &p, int cpuMask)
         p.intra_pred_new[0][BLOCK_16x16] = x265_intra_pred_planar16_new_sse4;
         p.intra_pred_new[0][BLOCK_32x32] = x265_intra_pred_planar32_new_sse4;
 
-        p.intra_pred_allangs[BLOCK_32x32] = x265_all_angs_pred_32x32_sse4;
-
         p.intra_pred_allangs_new[BLOCK_4x4] = x265_all_angs_pred_new_4x4_sse4;
         p.intra_pred_allangs_new[BLOCK_8x8] = x265_all_angs_pred_new_8x8_sse4;
         p.intra_pred_allangs_new[BLOCK_16x16] = x265_all_angs_pred_new_16x16_sse4;
+        p.intra_pred_allangs_new[BLOCK_32x32] = x265_all_angs_pred_new_32x32_sse4;
 
         p.intra_pred_new[1][BLOCK_4x4] = x265_intra_pred_dc4_new_sse4;
         p.intra_pred_new[1][BLOCK_8x8] = x265_intra_pred_dc8_new_sse4;
