21a0e937b47308ed87c4f584f2408ff5910cc3b3
Deepthi Nandakumar <deepthi@multicorewareinc.com>
2015-02-03 12:06:08 +0530

    level: choose high tier only if the determined level is greater than or equal to levelIdc.

--- source/encoder/level.cpp
+++ source/encoder/level.cpp
@@ -154,20 +154,26 @@ void determineLevel(const x265_param &param, VPS& vps)
             return;
         }
 
-        vps.ptl.levelIdc = levels[i].levelEnum;
-        vps.ptl.minCrForLevel = levels[i].minCompressionRatio;
-        vps.ptl.maxLumaSrForLevel = levels[i].maxLumaSamplesPerSecond;
-
 #define CHECK_RANGE(value, main, high) (value > main && value <= high)
 
         if (CHECK_RANGE(bitrate, levels[i].maxBitrateMain, levels[i].maxBitrateHigh) &&
             CHECK_RANGE((uint32_t)param.rc.vbvBufferSize, levels[i].maxCpbSizeMain, levels[i].maxCpbSizeHigh) &&
             levels[i].maxBitrateHigh != MAX_UINT)
-            vps.ptl.tierFlag = Level::HIGH;
+        {
+            /* If the user has not enabled high tier, continue looking to see if we can encode at a higher level, main tier */
+            if (!param.bHighTier && (levels[i].levelIdc < param.levelIdc))
+                continue;
+            else
+                vps.ptl.tierFlag = Level::HIGH;
+        }
         else
             vps.ptl.tierFlag = Level::MAIN;
         break;
 #undef CHECK_RANGE
+
+        vps.ptl.levelIdc = levels[i].levelEnum;
+        vps.ptl.minCrForLevel = levels[i].minCompressionRatio;
+        vps.ptl.maxLumaSrForLevel = levels[i].maxLumaSamplesPerSecond;
     }
 
     vps.ptl.intraConstraintFlag = false;
