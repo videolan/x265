23dc71fec417e29b5b4aa499925e284952bcb820
Sumalatha Polureddy <sumalatha@multicorewareinc.com>
2015-03-10 12:28:50 +0530

    asm: avx2 code for sad_x3[8x4] for 8bpp
    SSE3
    sad_x3[  8x4]  48.74x   234.10          11409.16
    
    AVX2
    sad_x3[  8x4]  50.47x   226.70          11442.29

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -1442,6 +1442,8 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // 8bpp
         p.pu[LUMA_8x16].satd  = x265_pixel_satd_8x16_avx2;
         p.pu[LUMA_8x8].satd   = x265_pixel_satd_8x8_avx2;
 
+        p.pu[LUMA_8x4].sad_x3 = x265_pixel_sad_x3_8x4_avx2;
+
         p.pu[LUMA_16x8].sad_x4  = x265_pixel_sad_x4_16x8_avx2;
         p.pu[LUMA_16x12].sad_x4 = x265_pixel_sad_x4_16x12_avx2;
         p.pu[LUMA_16x16].sad_x4 = x265_pixel_sad_x4_16x16_avx2;
