e6bb8da3a21b2d477284a34440273460699ea73e
Praveen Tiwari <praveen@multicorewareinc.com>
2015-03-13 14:02:25 +0530

    asm-intra_pred_ang16_28: improved, 865.16c -> 456.44c
    AVX2:
    intra_ang_16x16[28]     19.82x   456.44          9047.79
    
    SSE4:
    intra_ang_16x16[28]     10.36x   865.16          8962.21

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -1507,6 +1507,7 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // 8bpp
         p.cu[BLOCK_8x8].intra_pred[24] = x265_intra_pred_ang8_24_avx2;
         p.cu[BLOCK_8x8].intra_pred[11] = x265_intra_pred_ang8_11_avx2;
         p.cu[BLOCK_16x16].intra_pred[25] = x265_intra_pred_ang16_25_avx2;
+        p.cu[BLOCK_16x16].intra_pred[28] = x265_intra_pred_ang16_28_avx2;
 
         // copy_sp primitives
         p.cu[BLOCK_16x16].copy_sp = x265_blockcopy_sp_16x16_avx2;
--- source/common/x86/intrapred.h
+++ source/common/x86/intrapred.h
@@ -184,6 +184,7 @@ void x265_intra_pred_ang8_12_avx2(pixel* dst, intptr_t dstStride, const pixel* s
 void x265_intra_pred_ang8_24_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang8_11_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang16_25_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
+void x265_intra_pred_ang16_28_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_all_angs_pred_4x4_sse4(pixel *dest, pixel *refPix, pixel *filtPix, int bLuma);
 void x265_all_angs_pred_8x8_sse4(pixel *dest, pixel *refPix, pixel *filtPix, int bLuma);
 void x265_all_angs_pred_16x16_sse4(pixel *dest, pixel *refPix, pixel *filtPix, int bLuma);
