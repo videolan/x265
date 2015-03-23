cb42f2a2b0921f57cc1935f6d725f13d48279341
David T Yuen <dtyx265@gmail.com>
2015-03-23 13:18:33 -0700

    asm: intra_pred_ang4_7_sse2
    This is backported from sse4 code and replaces c code.
    
    64-bit
    
    ./test/TestBench --testbench intrapred | grep "intra_ang_4x4\[ 7\]"
    intra_ang_4x4[ 7]	2.77x 	 655.00   	 1817.47
    
    32-bit
    
    ./test/TestBench --testbench intrapred | grep "intra_ang_4x4\[ 7\]"
    intra_ang_4x4[ 7]	3.56x 	 762.50   	 2714.98

--- source/common/x86/intrapred8.asm
+++ source/common/x86/intrapred8.asm
@@ -1288,6 +1288,29 @@ cglobal intra_pred_ang4_6, 3,5,8
     mova        m7, [r3 +  1 * 16]  ; [20]
     jmp         mangle(private_prefix %+ _ %+ intra_pred_ang4_3 %+ SUFFIX %+ .do_filter4x4)
 
+cglobal intra_pred_ang4_7, 3,5,8
+    xor         r4, r4
+    inc         r4
+    cmp         r3m, byte 29
+    mov         r3, 9
+    cmove       r3, r4
+
+    movh        m0, [r2 + r3]    ; [8 7 6 5 4 3 2 1]
+    mova        m1, m0
+    psrldq      m1, 1           ; [x 8 7 6 5 4 3 2]
+    punpcklbw   m0, m1          ; [x 8 8 7 7 6 6 5 5 4 4 3 3 2 2 1]
+    mova        m3, m0
+    psrldq      m3, 2           ; [x x x x x x x x 6 5 5 4 4 3 3 2]
+    punpcklqdq  m2, m0, m3
+    punpcklqdq  m0, m0
+
+    lea         r3, [pw_ang_table + 20 * 16]
+    mova        m4, [r3 - 11 * 16]  ; [ 9]
+    mova        m5, [r3 -  2 * 16]  ; [18]
+    mova        m6, [r3 +  7 * 16]  ; [27]
+    mova        m7, [r3 - 16 * 16]  ; [ 4]
+    jmp         mangle(private_prefix %+ _ %+ intra_pred_ang4_3 %+ SUFFIX %+ .do_filter4x4)
+
 ;---------------------------------------------------------------------------------------------
 ; void intra_pred_dc(pixel* dst, intptr_t dstStride, pixel *srcPix, int dirMode, int bFilter)
 ;---------------------------------------------------------------------------------------------
