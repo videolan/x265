daf3fe10f029f3a9a58b84c4cf6ca5eeec950274
Praveen Tiwari <praveen@multicorewareinc.com>
2015-04-02 11:27:14 +0530

    asm: intra_pred_ang4_24 improved by ~38% over SSE4
    AVX2:
    intra_ang_4x4[24]       9.48x    90.10           854.38
    
    SSE4:
    intra_ang_4x4[24]       5.93x    145.08          860.60

--- source/common/x86/intrapred8.asm
+++ source/common/x86/intrapred8.asm
@@ -523,6 +523,7 @@ c_ang4_mode_30:          db 19, 13, 19, 13, 19, 13, 19, 13, 6, 26, 6, 26, 6, 26,
 c_ang4_mode_31:          db 15, 17, 15, 17, 15, 17, 15, 17, 30, 2, 30, 2, 30, 2, 30, 2, 13, 19, 13, 19, 13, 19, 13, 19, 28, 4, 28, 4, 28, 4, 28, 4
 c_ang4_mode_32:          db 11, 21, 11, 21, 11, 21, 11, 21, 22, 10, 22, 10, 22, 10, 22, 10, 1, 31, 1, 31, 1, 31, 1, 31, 12, 20, 12, 20, 12, 20, 12, 20
 c_ang4_mode_33:          db 6, 26, 6, 26, 6, 26, 6, 26, 12, 20, 12, 20, 12, 20, 12, 20, 18, 14, 18, 14, 18, 14, 18, 14, 24, 8, 24, 8, 24, 8, 24, 8
+c_ang4_mode_24:          db 5, 27, 5, 27, 5, 27, 5, 27, 10, 22, 10, 22, 10, 22, 10, 22, 15, 17, 15, 17, 15, 17, 15, 17, 20, 12, 20, 12, 20, 12, 20, 12
 
 ALIGN 32
 ;; (blkSize - 1 - x)
@@ -15577,6 +15578,22 @@ cglobal intra_pred_ang4_33, 3, 3, 1
     vbroadcasti128    m0, [r2 + 1]
     pshufb            m0, [intra_pred4_shuff33]
     pmaddubsw         m0, [c_ang4_mode_33]
+    pmulhrsw          m0, [pw_1024]
+    packuswb          m0, m0
+
+    movd              [r0], xm0
+    pextrd            [r0 + r1], xm0, 1
+    vextracti128      xm0, m0, 1
+    lea               r0, [r0 + 2 * r1]
+    movd              [r0], xm0
+    pextrd            [r0 + r1], xm0, 1
+    RET
+
+INIT_YMM avx2
+cglobal intra_pred_ang4_24, 3, 3, 1
+    vbroadcasti128    m0, [r2]
+    pshufb            m0, [intra_pred_shuff_0_4]
+    pmaddubsw         m0, [c_ang4_mode_24]
     pmulhrsw          m0, [pw_1024]
     packuswb          m0, m0
 
