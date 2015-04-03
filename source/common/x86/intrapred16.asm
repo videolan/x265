9b4aa687630bfb6681adef3807ea2d836fd9262d
David T Yuen <dtyx265@gmail.com>
2015-04-03 08:40:26 -0700

    asm: intra_pred_ang4_9_sse2 16-bit
    This is backported from sse4 code and replaces c code.
    
    ./test/TestBench --testbench intrapred | grep "intra_ang_4x4\[ 9\]"
    intra_ang_4x4[ 9]	4.33x 	 457.50   	 1982.47
    
    transposed mode
    
    ./test/TestBench --testbench intrapred | grep "intra_ang_4x4\[27\]"
    intra_ang_4x4[27]	3.61x 	 402.54   	 1453.19

--- source/common/x86/intrapred16.asm
+++ source/common/x86/intrapred16.asm
@@ -896,6 +896,27 @@ cglobal intra_pred_ang4_8, 3,5,8
     mova        m7, [r3 +  7 * 16]  ; [20]
     jmp         mangle(private_prefix %+ _ %+ intra_pred_ang4_3 %+ SUFFIX %+ .do_filter4x4)
 
+cglobal intra_pred_ang4_9, 3,5,8
+    mov         r4d, 2
+    cmp         r3m, byte 27
+    mov         r3d, 18
+    cmove       r3d, r4d
+
+    movu        m0, [r2 + r3]   ; [8 7 6 5 4 3 2 1]
+    mova        m2, m0
+    psrldq      m0, 2
+    punpcklwd   m2, m0      ; [5 4 4 3 3 2 2 1]
+    mova        m3, m2
+    mova        m4, m2
+    mova        m5, m2
+
+    lea         r3, [ang_table + 4 * 16]
+    mova        m0, [r3 -  2 * 16]  ; [ 2]
+    mova        m1, [r3 -  0 * 16]  ; [ 4]
+    mova        m6, [r3 +  2 * 16]  ; [ 6]
+    mova        m7, [r3 +  4 * 16]  ; [ 8]
+    jmp         mangle(private_prefix %+ _ %+ intra_pred_ang4_3 %+ SUFFIX %+ .do_filter4x4)
+
 ;-----------------------------------------------------------------------------------
 ; void intra_pred_dc(pixel* dst, intptr_t dstStride, pixel* above, int, int filter)
 ;-----------------------------------------------------------------------------------
