fd9f19e1d12c43ec847c076410078e7ca8b982e5
Praveen Tiwari <praveen@multicorewareinc.com>
2015-04-09 17:45:26 +0530

    asm: intra_pred_ang8_23 improved by ~18% over SSE4
    AVX2:
    intra_ang_8x8[23]       9.75x    205.43          2002.05
    
    SSE4:
    intra_ang_8x8[23]       8.12x    251.42          2041.61

--- source/common/x86/intrapred.h
+++ source/common/x86/intrapred.h
@@ -236,6 +236,7 @@ void x265_intra_pred_ang8_11_avx2(pixel* dst, intptr_t dstStride, const pixel* s
 void x265_intra_pred_ang8_13_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang8_14_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang8_15_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
+void x265_intra_pred_ang8_23_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang16_3_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang16_4_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
 void x265_intra_pred_ang16_5_avx2(pixel* dst, intptr_t dstStride, const pixel* srcPix, int dirMode, int bFilter);
