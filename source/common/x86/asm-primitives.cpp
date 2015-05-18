cd7f044dbbe6ca042f805e40af8ac7a3934d3595
Sumalatha Polureddy <devnull@localhost>
2015-05-18 12:32:04 +0530

    asm: avx2 code for sad_x3[64xN] for 10 bpp
    sse2
    sad_x3[64x16]  2.78x    8370.09         23242.03
    sad_x3[64x32]  2.67x    17362.56        46289.12
    sad_x3[64x48]  2.72x    25053.33        68260.15
    sad_x3[64x64]  2.47x    35227.60        87136.18
    
    avx2
    sad_x3[64x16]  6.45x    3664.96         23624.50
    sad_x3[64x32]  5.74x    8741.48         50144.03
    sad_x3[64x48]  6.07x    11401.75        69182.98
    sad_x3[64x64]  6.38x    16092.67        102696.92

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -1340,6 +1340,10 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // 16bpp
         p.pu[LUMA_32x24].sad_x3 = x265_pixel_sad_x3_32x24_avx2;
         p.pu[LUMA_32x32].sad_x3 = x265_pixel_sad_x3_32x32_avx2;
         p.pu[LUMA_32x64].sad_x3 = x265_pixel_sad_x3_32x64_avx2;
+        p.pu[LUMA_64x16].sad_x3 = x265_pixel_sad_x3_64x16_avx2;
+        p.pu[LUMA_64x32].sad_x3 = x265_pixel_sad_x3_64x32_avx2;
+        p.pu[LUMA_64x48].sad_x3 = x265_pixel_sad_x3_64x48_avx2;
+        p.pu[LUMA_64x64].sad_x3 = x265_pixel_sad_x3_64x64_avx2;
 
         p.pu[LUMA_16x4].convert_p2s = x265_filterPixelToShort_16x4_avx2;
         p.pu[LUMA_16x8].convert_p2s = x265_filterPixelToShort_16x8_avx2;
