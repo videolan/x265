a44330660ad1478fbca12b7a5337b16b14de3f22
Min Chen <devnull@localhost>
2015-06-22 13:57:06 -0500

    cmake: introduce multilib support for MSVC
    Note, the multilib configuration exposes a bug in MSVC 2008 in that the
    anonymous namespaced functions are not truly file local, the unique namespaced
    functions are exported and thus collide with other libx265 static libraries. It
    is not known if later MSVC compilers are also affected.
    
    To work around this, we tell the linker to ignore duplicate symbols. This seems
    to work but is not a terribly robust long-term solution.

---
+++ build/vc11-x86_64/multilib.bat
@@ -0,0 +1,34 @@
+@echo off
+if "%VS110COMNTOOLS%" == "" (
+  msg "%username%" "Visual Studio 11 not detected"
+  exit 1
+)
+
+@mkdir 10bpp
+@mkdir 8bpp
+
+@cd 10bpp
+if not exist x265.sln (
+  cmake  -G "Visual Studio 11 Win64" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DWINXP_SUPPORT=ON
+)
+if exist x265.sln (
+  call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat"
+  MSBuild /property:Configuration="Release" x265.sln
+  copy/y Release\x265-static.lib ..\8bpp\x265-static-10bpp.lib
+)
+@cd ..
+
+@cd 8bpp
+if not exist x265-static-10bpp.lib (
+  msg "%username%" "10bpp build failured"
+  exit 1
+)
+if not exist x265.sln (
+  cmake  -G "Visual Studio 11 Win64" ../../../source -DHIGH_BIT_DEPTH=OFF -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=ON -DWINXP_SUPPORT=ON -DEXTRA_LIB=x265-static-10bpp.lib -DEXTRA_LINK_FLAGS="/FORCE:MULTIPLE"
+)
+if exist x265.sln (
+  call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat"
+  MSBuild /property:Configuration="Release" x265.sln
+  copy/y Release\x265.exe ..
+)
+@cd ..
