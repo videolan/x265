a8150e02c5a38201a813816440fd815b868ad892
Steve Borho <steve@borho.org>
2015-06-22 20:32:39 -0500

    cmake: change msvc multilib build folders to 8bit/10bit

--- build/vc9-x86_64/multilib.bat
+++ build/vc9-x86_64/multilib.bat
@@ -4,27 +4,27 @@ if "%VS90COMNTOOLS%" == "" (
   exit 1
 )
 
-@mkdir 10bpp
-@mkdir 8bpp
+@mkdir 10bit
+@mkdir 8bit
 
-@cd 10bpp
+@cd 10bit
 if not exist x265.sln (
   cmake  -G "Visual Studio 9 2008 Win64" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DWINXP_SUPPORT=ON
 )
 if exist x265.sln (
   call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat"
   MSBuild /property:Configuration="Release" x265.sln
-  copy/y Release\x265-static.lib ..\8bpp\x265-static-10bpp.lib
+  copy/y Release\x265-static.lib ..\8bit\x265-static-main10.lib
 )
 @cd ..
 
-@cd 8bpp
-if not exist x265-static-10bpp.lib (
-  msg "%username%" "10bpp build failured"
+@cd 8bit
+if not exist x265-static-main10.lib (
+  msg "%username%" "main10 build failured"
   exit 1
 )
 if not exist x265.sln (
-  cmake  -G "Visual Studio 9 2008 Win64" ../../../source -DHIGH_BIT_DEPTH=OFF -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=ON -DWINXP_SUPPORT=ON -DEXTRA_LIB=x265-static-10bpp.lib -DEXTRA_LINK_FLAGS="/FORCE:MULTIPLE"
+  cmake  -G "Visual Studio 9 2008 Win64" ../../../source -DHIGH_BIT_DEPTH=OFF -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=ON -DWINXP_SUPPORT=ON -DEXTRA_LIB=x265-static-main10.lib -DEXTRA_LINK_FLAGS="/FORCE:MULTIPLE"
 )
 if exist x265.sln (
   call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat"
