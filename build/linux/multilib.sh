ad2310f7b8065a7bae8d3645487145cea702a870
Steve Borho <steve@borho.org>
2015-07-08 13:35:39 -0500

    cmake: keep all linker options in list, fixes multilib link

--- build/linux/multilib.sh
+++ build/linux/multilib.sh
@@ -13,5 +13,5 @@ make ${MAKEFLAGS}
 cd ../8bit
 ln -sf ../10bit/libx265.a libx265_main10.a
 ln -sf ../12bit/libx265.a libx265_main12.a
-cmake ../../../source -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON -DENABLE_SHARED=OFF
+cmake ../../../source -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON
 make ${MAKEFLAGS}
