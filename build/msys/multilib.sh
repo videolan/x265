2f722e0a30c6f288ef1558d69abe5939f67bc799
Steve Borho <steve@borho.org>
2015-07-09 13:07:52 -0500

    cmake: msys includes GNU ar and can use the same approach as Linux

--- build/msys/multilib.sh
+++ build/msys/multilib.sh
@@ -15,3 +15,15 @@ cp libx265.a ../8bit/libx265_main10.a
 cd ../8bit
 cmake -G "MSYS Makefiles" ../../../source -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON
 make ${MAKEFLAGS}
+
+# rename the 8bit library, then combine all three into libx265.a using GNU ar
+mv libx265.a libx265_main.a
+
+ar -M <<EOF
+CREATE libx265.a
+ADDLIB libx265_main.a
+ADDLIB libx265_main10.a
+ADDLIB libx265_main12.a
+SAVE
+END
+EOF
