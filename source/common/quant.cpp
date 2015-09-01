299af6c38e34a407e8ad680e774e1457de30bda7
Ashok Kumar Mishra <ashok@multicorewareinc.com>
2015-09-01 13:50:26 +0530

    Quant: fix for non-deterministic ouput in lossless mode - issue #173

--- source/common/quant.cpp
+++ source/common/quant.cpp
@@ -228,6 +228,8 @@ Quant::~Quant()
 
 void Quant::setQPforQuant(const CUData& ctu, int qp)
 {
+    m_tqBypass = !!ctu.m_tqBypass[0];
+
     m_nr = m_frameNr ? &m_frameNr[ctu.m_encData->m_frameEncoderID] : NULL;
     m_qpParam[TEXT_LUMA].setQpParam(qp + QP_BD_OFFSET);
     setChromaQP(qp + ctu.m_slice->m_pps->chromaQpOffset[0], TEXT_CHROMA_U, ctu.m_chromaFormat);
