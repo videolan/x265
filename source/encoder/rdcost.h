d1ffb2889eee82bed7b885fa16abfe51a8982bdc
Min Chen <chenm003@163.com>
2015-09-18 18:53:46 -0500

    rdcost: fix checked build warnings, nits

--- source/encoder/rdcost.h
+++ source/encoder/rdcost.h
@@ -90,13 +90,15 @@ public:
 
     inline uint64_t calcRdCost(sse_ret_t distortion, uint32_t bits) const
     {
-        X265_CHECK(bits <= (UINT64_MAX - 128) / m_lambda2,
 #if X265_DEPTH <= 10
+        X265_CHECK(bits <= (UINT64_MAX - 128) / m_lambda2,
                    "calcRdCost wrap detected dist: %u, bits %u, lambda: " X265_LL "\n",
-#else
-                   "calcRdCost wrap detected dist: " X265_LL ", bits %u, lambda: " X265_LL "\n",
-#endif
                    distortion, bits, m_lambda2);
+#else
+        X265_CHECK(bits <= (UINT64_MAX - 128) / m_lambda2,
+                   "calcRdCost wrap detected dist: " X265_LL ", bits %u, lambda: " X265_LL "\n",
+                   distortion, bits, m_lambda2);
+#endif
         return distortion + ((bits * m_lambda2 + 128) >> 8);
     }
 
@@ -127,13 +129,15 @@ public:
 
     inline sse_ret_t scaleChromaDist(uint32_t plane, sse_ret_t dist) const
     {
-        X265_CHECK(dist <= (UINT64_MAX - 128) / m_chromaDistWeight[plane - 1],
 #if X265_DEPTH <= 10
+        X265_CHECK(dist <= (UINT64_MAX - 128) / m_chromaDistWeight[plane - 1],
                    "scaleChromaDist wrap detected dist: %u, lambda: %u\n",
-#else
-                   "scaleChromaDist wrap detected dist: " X265_LL " lambda: %u\n",
-#endif
                    dist, m_chromaDistWeight[plane - 1]);
+#else
+        X265_CHECK(dist <= (UINT64_MAX - 128) / m_chromaDistWeight[plane - 1],
+                   "scaleChromaDist wrap detected dist: " X265_LL " lambda: %u\n",
+                   dist, m_chromaDistWeight[plane - 1]);
+#endif
         return (sse_ret_t)((dist * (uint64_t)m_chromaDistWeight[plane - 1] + 128) >> 8);
     }
 
