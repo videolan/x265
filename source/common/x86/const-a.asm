09baba8bc479c393a09700aaafdabe395bd5aff9
Min Chen <chenm003@163.com>
2015-10-19 17:40:21 -0500

    asm: extend constant 0x3FFF to 256-bits

--- source/common/x86/const-a.asm
+++ source/common/x86/const-a.asm
@@ -93,7 +93,7 @@ const pw_00ff,              times 16 dw 0x00ff
 const pw_ff00,              times  8 dw 0xff00
 const pw_2000,              times 16 dw 0x2000
 const pw_8000,              times  8 dw 0x8000
-const pw_3fff,              times  8 dw 0x3fff
+const pw_3fff,              times 16 dw 0x3fff
 const pw_32_0,              times  4 dw 32,
                             times  4 dw 0
 const pw_pixel_max,         times 16 dw ((1 << BIT_DEPTH)-1)
--- source/common/x86/mc-a2.asm
+++ source/common/x86/mc-a2.asm
@@ -1059,8 +1059,7 @@ cglobal mbtree_propagate_cost, 6,6,7
     vbroadcastsd    m6, [r5]
     mulpd           m6, [pd_inv256]
     xor             r5d, r5d
-    lea             r0, [r0+r5*2]
-    mova            m5, [pw_3fff]
+    vbroadcasti128  m5, [pw_3fff]
 
 .loop:
     movu            xm2, [r2+r5*4]      ; intra
