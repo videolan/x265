50cb28345f7e19e2be5bdd4b9ccdb3ddec906f39
Dnyaneshwar G <dnyaneshwar@multicorewareinc.com>
2016-02-09 15:06:29 +0530

    threadpool: utilize all processors on embedded ARM platforms

--- source/common/threadpool.cpp
+++ source/common/threadpool.cpp
@@ -528,6 +528,10 @@ int ThreadPool::getCpuCount()
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
     return sysinfo.dwNumberOfProcessors;
+#elif __unix__ && X265_ARCH_ARM
+    /* Return the number of processors configured by OS. Because, most embedded linux distributions
+     * uses only one processor as the scheduler doesn't have enough work to utilize all processors */
+    return sysconf(_SC_NPROCESSORS_CONF);
 #elif __unix__
     return sysconf(_SC_NPROCESSORS_ONLN);
 #elif MACOS
