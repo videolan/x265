aff756167872d22f32583bd8cee85a73db45cc9c
Min Chen <min.chen@multicorewareinc.com>
2016-10-24 16:38:55 -0500

    [PATCH] [PPC] GPL v2 copyright header [x265,PPC] GPL v2 copyright header >From 1bea85513646e4d9d992bbe326a9cb3275ec313a Mon Sep 17 00:00:00 2001

--- source/common/ppc/ipfilter_altivec.cpp
+++ source/common/ppc/ipfilter_altivec.cpp
@@ -1,13 +1,33 @@
-#include <iostream>
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Roger Moussalli <rmoussal@us.ibm.com>
+ *          Min Chen <min.chen@multicorewareinc.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at license @ x265.com.
+ *****************************************************************************/
 
+#include <iostream>
 #include "common.h"
 #include "primitives.h"
 #include "ppccommon.h"
 
 using namespace X265_NS;
-
-
-
 
 // ORIGINAL : for(col=0; col<16; col++) {sum[col]  = src[ocol+col + 0 * srcStride] * c[0];}
 #define multiply_pixel_coeff(/*vector int*/ v_sum_0, /*vector int*/ v_sum_1, /*vector int*/ v_sum_2, /*vector int*/ v_sum_3, /*const pixel * */ src, /*int*/ src_offset, /*vector signed short*/ v_coeff) \
