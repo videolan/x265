7b6da02922701aa2d146a379e5391e0687b2ab2a
Pradeep Ramachandran <pradeep@multicorewareinc.com>
2016-12-30 09:20:28 +0530

    warn: Fix warning from x265cli.h

--- source/x265cli.h
+++ source/x265cli.h
@@ -344,7 +344,7 @@ static void showHelp(x265_param *param)
     H0("   --[no-]psy-rd <0..5.0>        Strength of psycho-visual rate distortion optimization, 0 to disable. Default %.1f\n", param->psyRd);
     H0("   --[no-]rdoq-level <0|1|2>     Level of RDO in quantization 0:none, 1:levels, 2:levels & coding groups. Default %d\n", param->rdoqLevel);
     H0("   --[no-]psy-rdoq <0..50.0>     Strength of psycho-visual optimization in RDO quantization, 0 to disable. Default %.1f\n", param->psyRdoq);
-    H0("   --[no-]ssim-rd                Enable ssim rate distortion optimization, 0 to disable. Default %.1f\n", OPT(param->bSsimRd));
+    H0("   --[no-]ssim-rd                Enable ssim rate distortion optimization, 0 to disable. Default %s\n", OPT(param->bSsimRd));
     H0("   --[no-]rd-refine              Enable QP based RD refinement for rd levels 5 and 6. Default %s\n", OPT(param->bEnableRdRefine));
     H0("   --[no-]early-skip             Enable early SKIP detection. Default %s\n", OPT(param->bEnableEarlySkip));
     H0("   --[no-]rskip                  Enable early exit from recursion. Default %s\n", OPT(param->bEnableRecursionSkip));
