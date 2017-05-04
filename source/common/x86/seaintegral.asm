d3f9a1242ff1051028ddf21566be9cae1960f6a1
Praveen Tiwari <praveen@multicorewareinc.com>
2017-05-04 19:13:48 +0530

    avx2: 'integral4v' asm code -> 7.48x faster than 'C' version
       integral_init4v  7.48x    202.53          1515.14

--- source/common/x86/seaintegral.asm
+++ source/common/x86/seaintegral.asm
@@ -32,8 +32,19 @@ SECTION .text
 ;void integral_init4v_c(uint32_t *sum4, intptr_t stride)
 ;-----------------------------------------------------------------------------
 INIT_YMM avx2
-cglobal integral4v, 2, 2, 0
- 
+cglobal integral4v, 2, 3, 2
+    mov r2, r1
+    shl r2, 4
+
+.loop
+    movu    m0, [r0]
+    movu    m1, [r0 + r2]
+    psubd   m1, m0
+    movu    [r0], m1
+    add     r0, 32
+    sub     r1, 8
+    cmp     r1, 0
+    jnz     .loop
     RET
 
 ;-----------------------------------------------------------------------------
