c40689f2ffc74a41624f3913c03fbc79394b13be
Gopi Satykrishna Akisetty <gopi.satykrishna@multicorewareinc.com>
2017-07-28 16:30:23 +0530

    [x265-avx512]x86: AVX512 pixel_sad_32xN for high bit depth
    Size    | AVX2 performance | AVX512 performance
    ------------------------------------------------
    32x8    |     27.57x       |      35.17x
    32x16   |     27.96x       |      40.74x
    32x24   |     31.21x       |      45.19x
    32x32   |     32.12x       |      47.23x
    32x64   |     28.79x       |      53.35x

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -2262,6 +2262,12 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // Main10
         p.pu[LUMA_32x32].sad = PFX(pixel_sad_32x32_avx512);
         p.pu[LUMA_32x64].sad = PFX(pixel_sad_32x64_avx512);
 
+        p.pu[LUMA_32x8].sad = PFX(pixel_sad_32x8_avx512);
+        p.pu[LUMA_32x16].sad = PFX(pixel_sad_32x16_avx512);
+        p.pu[LUMA_32x24].sad = PFX(pixel_sad_32x24_avx512);
+        p.pu[LUMA_32x32].sad = PFX(pixel_sad_32x32_avx512);
+        p.pu[LUMA_32x64].sad = PFX(pixel_sad_32x64_avx512);
+
     }
 }
 #else // if HIGH_BIT_DEPTH
