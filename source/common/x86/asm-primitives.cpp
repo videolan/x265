45fe4e8dd1f49b925db6a26428820d05eaed3776
Gopi Satykrishna Akisetty <gopi.satykrishna@multicorewareinc.com>
2017-08-07 16:30:18 +0530

    [x265-avx512]x86: clean up line endings issue in sad16-a.asm and asm-primitives.cpp files.

--- source/common/x86/asm-primitives.cpp
+++ source/common/x86/asm-primitives.cpp
@@ -2267,17 +2267,6 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // Main10
         p.pu[LUMA_64x48].sad = PFX(pixel_sad_64x48_avx512);
         p.pu[LUMA_64x64].sad = PFX(pixel_sad_64x64_avx512);
 
-        p.pu[LUMA_32x8].sad = PFX(pixel_sad_32x8_avx512);
-        p.pu[LUMA_32x16].sad = PFX(pixel_sad_32x16_avx512);
-        p.pu[LUMA_32x24].sad = PFX(pixel_sad_32x24_avx512);
-        p.pu[LUMA_32x32].sad = PFX(pixel_sad_32x32_avx512);
-        p.pu[LUMA_32x64].sad = PFX(pixel_sad_32x64_avx512);
-        p.pu[LUMA_48x64].sad = PFX(pixel_sad_48x64_avx512);
-        p.pu[LUMA_64x16].sad = PFX(pixel_sad_64x16_avx512);
-        p.pu[LUMA_64x32].sad = PFX(pixel_sad_64x32_avx512);
-        p.pu[LUMA_64x48].sad = PFX(pixel_sad_64x48_avx512);
-        p.pu[LUMA_64x64].sad = PFX(pixel_sad_64x64_avx512);
-
         p.pu[LUMA_64x16].addAvg = PFX(addAvg_64x16_avx512);
         p.pu[LUMA_64x32].addAvg = PFX(addAvg_64x32_avx512);
         p.pu[LUMA_64x48].addAvg = PFX(addAvg_64x48_avx512);
@@ -2312,18 +2301,6 @@ void setupAssemblyPrimitives(EncoderPrimitives &p, int cpuMask) // Main10
         p.pu[LUMA_32x24].sad_x3 = PFX(pixel_sad_x3_32x24_avx512);
         p.pu[LUMA_32x32].sad_x3 = PFX(pixel_sad_x3_32x32_avx512);
         p.pu[LUMA_32x64].sad_x3 = PFX(pixel_sad_x3_32x64_avx512);
-
-        p.pu[LUMA_32x8].sad_x3 = PFX(pixel_sad_x3_32x8_avx512);
-        p.pu[LUMA_32x16].sad_x3 = PFX(pixel_sad_x3_32x16_avx512);
-        p.pu[LUMA_32x24].sad_x3 = PFX(pixel_sad_x3_32x24_avx512);
-        p.pu[LUMA_32x32].sad_x3 = PFX(pixel_sad_x3_32x32_avx512);
-        p.pu[LUMA_32x64].sad_x3 = PFX(pixel_sad_x3_32x64_avx512);
-
-        p.pu[LUMA_32x8].sad_x4 = PFX(pixel_sad_x4_32x8_avx512);
-        p.pu[LUMA_32x16].sad_x4 = PFX(pixel_sad_x4_32x16_avx512);
-        p.pu[LUMA_32x24].sad_x4 = PFX(pixel_sad_x4_32x24_avx512);
-        p.pu[LUMA_32x32].sad_x4 = PFX(pixel_sad_x4_32x32_avx512);
-        p.pu[LUMA_32x64].sad_x4 = PFX(pixel_sad_x4_32x64_avx512);
 
         p.pu[LUMA_32x8].sad_x4 = PFX(pixel_sad_x4_32x8_avx512);
         p.pu[LUMA_32x16].sad_x4 = PFX(pixel_sad_x4_32x16_avx512);
