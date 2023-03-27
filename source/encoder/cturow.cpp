4180b2344775389ca85f7fb57a184833b8110a51
Law berry <law.berry@aol.com>
2023-03-28 03:38:00 +0800

    change license email globally
    Conflicts:
    	source/common/vec/blockcopy-sse3.cpp
    	source/encoder/compress.cpp
    	source/encoder/cturow.cpp
    	source/encoder/cturow.h
    	source/filters/filters.cpp
    	source/test/testpool.cpp

---
+++ source/encoder/cturow.cpp
@@ -0,0 +1,118 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Chung Shin Yee <shinyee@multicorewareinc.com>
+ *          Min Chen <chenm003@163.com>
+ *          Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at license @ x265.com.
+ *****************************************************************************/
+
+#include "encoder.h"
+#include "PPA/ppa.h"
+#include "cturow.h"
+
+using namespace x265;
+
+bool CTURow::create(Encoder* top)
+{
+    m_rdGoOnSbacCoder.init(&m_rdGoOnBinCodersCABAC);
+    m_sbacCoder.init(&m_binCoderCABAC);
+    m_trQuant.init(1 << top->m_quadtreeTULog2MaxSize, top->bEnableRDOQ, top->bEnableRDOQTS, !!top->param->bEnableTSkipFast);
+    m_rdCost.setPsyRdScale(top->param->rdLevel >= 5 ? top->param->psyRd : 0);
+    m_rdSbacCoders = new TEncSbac * *[g_maxCUDepth + 1];
+    m_binCodersCABAC = new TEncBinCABAC * *[g_maxCUDepth + 1];
+
+    for (uint32_t depth = 0; depth < g_maxCUDepth + 1; depth++)
+    {
+        m_rdSbacCoders[depth]  = new TEncSbac*[CI_NUM];
+        m_binCodersCABAC[depth] = new TEncBinCABAC*[CI_NUM];
+
+        for (int ciIdx = 0; ciIdx < CI_NUM; ciIdx++)
+        {
+            m_rdSbacCoders[depth][ciIdx] = new TEncSbac;
+            m_binCodersCABAC[depth][ciIdx] = new TEncBinCABAC(true);
+            m_rdSbacCoders[depth][ciIdx]->init(m_binCodersCABAC[depth][ciIdx]);
+        }
+    }
+
+    m_cuCoder.init(top);
+    m_cuCoder.setRdCost(&m_rdCost);
+    m_cuCoder.setRDSbacCoder(m_rdSbacCoders);
+    m_cuCoder.setEntropyCoder(&m_entropyCoder);
+    m_cuCoder.setPredSearch(&m_search);
+    m_cuCoder.setTrQuant(&m_trQuant);
+    m_cuCoder.setRdCost(&m_rdCost);
+    m_search.setRDSbacCoder(m_rdSbacCoders);
+    m_search.setEntropyCoder(&m_entropyCoder);
+    m_search.setRDGoOnSbacCoder(&m_rdGoOnSbacCoder);
+
+    return m_search.init(top, &m_rdCost, &m_trQuant) &&
+           m_cuCoder.create((uint8_t)g_maxCUDepth, g_maxCUSize);
+}
+
+void CTURow::processCU(TComDataCU *cu, TComSlice *slice, TEncSbac *bufferSbac, bool bSaveSBac)
+{
+    if (bufferSbac)
+    {
+        // Load SBAC coder context from previous row.
+        m_rdSbacCoders[0][CI_CURR_BEST]->loadContexts(bufferSbac);
+    }
+
+    m_entropyCoder.setEntropyCoder(&m_rdGoOnSbacCoder, slice);
+    m_entropyCoder.setBitstream(&m_bitCounter);
+    m_cuCoder.setRDGoOnSbacCoder(&m_rdGoOnSbacCoder);
+
+    m_cuCoder.compressCU(cu); // Does all the CU analysis
+
+    // restore entropy coder to an initial state
+    m_entropyCoder.setEntropyCoder(m_rdSbacCoders[0][CI_CURR_BEST], slice);
+    m_entropyCoder.setBitstream(&m_bitCounter);
+    m_cuCoder.setBitCounter(&m_bitCounter);
+    m_bitCounter.resetBits();
+
+    m_cuCoder.encodeCU(cu);  // Count bits
+
+    if (bSaveSBac)
+    {
+        // Save CABAC state for next row
+        m_bufferSbacCoder.loadContexts(m_rdSbacCoders[0][CI_CURR_BEST]);
+    }
+}
+
+void CTURow::destroy()
+{
+    for (uint32_t depth = 0; depth < g_maxCUDepth + 1; depth++)
+    {
+        for (int ciIdx = 0; ciIdx < CI_NUM; ciIdx++)
+        {
+            delete m_rdSbacCoders[depth][ciIdx];
+            delete m_binCodersCABAC[depth][ciIdx];
+        }
+    }
+
+    for (uint32_t depth = 0; depth < g_maxCUDepth + 1; depth++)
+    {
+        delete [] m_rdSbacCoders[depth];
+        delete [] m_binCodersCABAC[depth];
+    }
+
+    delete[] m_rdSbacCoders;
+    delete[] m_binCodersCABAC;
+    m_cuCoder.destroy();
+}
---
+++ source/encoder/cturow.h
@@ -0,0 +1,112 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Chung Shin Yee <shinyee@multicorewareinc.com>
+ *          Min Chen <chenm003@163.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at license @ x265.com.
+ *****************************************************************************/
+
+#ifndef X265_CTUROW_H
+#define X265_CTUROW_H
+
+#include "TLibCommon/TComBitCounter.h"
+#include "TLibCommon/TComPic.h"
+
+#include "TLibEncoder/TEncCu.h"
+#include "TLibEncoder/TEncSearch.h"
+#include "TLibEncoder/TEncSbac.h"
+#include "TLibEncoder/TEncBinCoderCABAC.h"
+
+#include "rdcost.h"
+
+namespace x265 {
+// private x265 namespace
+
+class Encoder;
+
+/* manages the state of encoding one row of CTU blocks.  When
+ * WPP is active, several rows will be simultaneously encoded.
+ * When WPP is inactive, only one CTURow instance is used. */
+class CTURow
+{
+public:
+
+    CTURow() : m_rdGoOnBinCodersCABAC(true) {}
+
+    TEncSbac               m_sbacCoder;
+    TEncSbac               m_rdGoOnSbacCoder;
+    TEncSbac               m_bufferSbacCoder;
+    TEncBinCABAC           m_binCoderCABAC;
+    TEncBinCABAC           m_rdGoOnBinCodersCABAC;
+    TComBitCounter         m_bitCounter;
+    RDCost                 m_rdCost;
+    TEncEntropy            m_entropyCoder;
+    TEncSearch             m_search;
+    TEncCu                 m_cuCoder;
+    TComTrQuant            m_trQuant;
+    TEncSbac            ***m_rdSbacCoders;
+    TEncBinCABAC        ***m_binCodersCABAC;
+
+    bool create(Encoder* top);
+
+    void destroy();
+
+    void init(TComSlice *slice)
+    {
+        m_active = 0;
+
+        // Note: Reset status to avoid frame parallelism output mistake on different thread number
+        for (uint32_t depth = 0; depth < g_maxCUDepth + 1; depth++)
+        {
+            for (int ciIdx = 0; ciIdx < CI_NUM; ciIdx++)
+            {
+                m_rdSbacCoders[depth][ciIdx]->setSlice(slice);
+                m_rdSbacCoders[depth][ciIdx]->resetEntropy();
+                m_binCodersCABAC[depth][ciIdx]->m_fracBits = 0;
+            }
+        }
+
+        m_rdGoOnSbacCoder.setSlice(slice);
+        m_rdGoOnSbacCoder.resetEntropy();
+    }
+
+    void processCU(TComDataCU *cu, TComSlice *slice, TEncSbac *bufferSBac, bool bSaveCabac);
+
+    /* Threading variables */
+
+    /* This lock must be acquired when reading or writing m_active or m_busy */
+    Lock                m_lock;
+
+    /* row is ready to run, has no neighbor dependencies. The row may have
+     * external dependencies (reference frame pixels) that prevent it from being
+     * processed, so it may stay with m_active=true for some time before it is
+     * encoded by a worker thread. */
+    volatile bool       m_active;
+
+    /* row is being processed by a worker thread.  This flag is only true when a
+     * worker thread is within the context of FrameEncoder::processRow(). This
+     * flag is used to detect multiple possible wavefront problems. */
+    volatile bool       m_busy;
+
+    /* count of completed CUs in this row */
+    volatile uint32_t   m_completed;
+};
+}
+
+#endif // ifndef X265_CTUROW_H
